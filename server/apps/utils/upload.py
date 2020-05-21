66c9cff92003fe93ae9260d2c74a0d4553745190
Robin TROESCH <38283096+RobinetFox@users.noreply.github.com>
2020-05-20 22:22:49 -0700

    add logo for groups

---
+++ server/apps/utils/upload.py
@@ -0,0 +1,20 @@
+import os
+from uuid import uuid4
+from django.utils.deconstruct import deconstructible
+
+@deconstructible
+class PathAndRename(object):
+
+    def __init__(self, sub_path):
+        self.path = sub_path
+
+    def __call__(self, instance, filename):
+        ext = filename.split('.')[-1]
+        # get filename
+        if instance.pk:
+            filename = f'{instance.pk}.{ext}'
+        else:
+            # set filename as random string
+            filename = f'{uuid4().hex}.{ext}'
+        # return the whole path to the file
+        return os.path.join(self.path, filename)
