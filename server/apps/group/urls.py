397d1e845db0deb8df1873ca2ff8adef0279f121
Robin TROESCH <38283096+RobinetFox@users.noreply.github.com>
2020-05-22 00:09:00 -0700

    split of the admin page for a group

--- server/apps/group/urls.py
+++ server/apps/group/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('<slug:pk>/', DetailClubView.as_view(), name='detail'),
     path('<slug:pk>/edit', UpdateClubView.as_view(), name='update'),
     path('<slug:group_slug>/member/add/<slug:user_id>', add_member, name='add-member'),
-    path('<slug:pk>/members/edit', edit_named_memberships, name='editNamedMemberships'),
     path('', ListClubView.as_view(), name='list'),
-    path('<slug:group_slug>/events/edit', UpdateClubEventsView.as_view(), name='update-events')
+    path('<slug:group_slug>/events/edit', UpdateGroupEventsView.as_view(), name='update-events'),
+    path('<slug:group_slug>/members/edit', UpdateGroupMembersView.as_view(), name='update-members')
 ]
