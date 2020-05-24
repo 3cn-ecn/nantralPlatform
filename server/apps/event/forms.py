8bbb41b56db64845595bce2d8e6d7b05f850f685
Robin TROESCH <38283096+RobinetFox@users.noreply.github.com>
2020-05-23 18:01:04 -0700

    add events on home page and club detail page

--- server/apps/event/forms.py
+++ server/apps/event/forms.py
@@ -3,17 +3,21 @@ from django.forms import modelformset_factory
 
 from apps.event.models import Event
 
-class EventGroupForm(forms.ModelForm):
+class EventForm(forms.ModelForm):
     model = Event
     fields = ['title', 'description', 'location', 'date', 'publicity']
+    def __init__(self, *args, **kwargs):
+        super(EventForm, self).__init__(*args, **kwargs)
+        self.fields['date'] = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'])
+        self.fields['date'].widget.attrs['class'] = 'datepicker'
 
 
-EventGroupFormSet = modelformset_factory(
+EventFormSet = modelformset_factory(
     Event,
     fields=['title', 'description', 'location', 'date', 'publicity'],
     extra=1,
     can_delete=True,
     widgets= {
-            'date': forms.DateInput(attrs={'type':'date'})
+            'date': forms.DateTimeInput(attrs={'class':'datepicker'})
         }
 )
--- server/apps/home/views.py
+++ server/apps/home/views.py
@@ -3,16 +3,13 @@ from django.views.generic import TemplateView
 from urllib import parse
 from ..utils.accessMixins import LoginRequiredAccessMixin
 
+from apps.event.models import Event
+
 # Create your views here.
 class HomeView(LoginRequiredAccessMixin, TemplateView):
     template_name = 'home/home.html'
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        if self.request.GET.get('message_type') is not None:
-            message = {
-                'type': self.request.GET.get('message_type'),
-                'text': parse.unquote(self.request.GET.get('message_text'))
-            }
-            context['message'] = message
+        context['events'] = Event.objects.all()
         return context
