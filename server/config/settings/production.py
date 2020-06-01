51e5f9a24346645e0aedc81b80a8bd939049467e
Robin TROESCH <38283096+RobinetFox@users.noreply.github.com>
2020-05-31 21:38:31 -0700

    allowed host

--- server/config/settings/production.py
+++ server/config/settings/production.py
@@ -45,7 +45,7 @@ AWS_S3_REGION_NAME = 'eu-west-3'
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 EMAIL_BACKEND = 'django_amazon_ses.EmailBackend'
 
