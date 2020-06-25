e0835b775b3c339577600926ace74c5ef01435d8
Robin TROESCH <38283096+RobinetFox@users.noreply.github.com>
2020-06-25 14:58:43 -0700

    add an event view

--- server/apps/event/views.py
+++ server/apps/event/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+from .models import *
+
+class EventDetailView(TemplateView):
+    template_name = 'event/event_detail.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.object = BaseEvent.get_event_by_slug(self.kwargs['event_slug'])
+        context['object'] = self.object
+        context['group'] = self.object.get_group
+        context['is_participating'] = self.object.is_participating
+        return context
