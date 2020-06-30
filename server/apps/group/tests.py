0d86cadc849ba4e8b1445f7a64762fa605327768
Robin TROESCH <38283096+RobinetFox@users.noreply.github.com>
2020-06-29 17:55:02 -0700

    add test on adding a member

--- server/apps/group/tests.py
+++ server/apps/group/tests.py
@@ -3,7 +3,7 @@ from apps.utils.utest import TestMixin
 from django.urls import reverse
 from rest_framework import status
 
-from .models import Club
+from .models import Club, NamedMembershipClub
 
 class TestGroups(TestCase, TestMixin):
     def setUp(self):
@@ -16,7 +16,6 @@ class TestGroups(TestCase, TestMixin):
     def test_club_views(self):
         Club.objects.create(name='TestClub')
         club = Club.objects.all().first()
-        print(club)
         url = reverse('group:detail', args=[club.slug])
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
@@ -26,5 +25,18 @@ class TestGroups(TestCase, TestMixin):
         self.assertEqual(resp.status_code, status.HTTP_302_FOUND)
 
     def test_add_member(self):
-        pass
+        club = Club.objects.create(name='TestClub')
+        payload = {
+            'function': 'test',
+            'year': 2020
+        }
+        url = reverse('group:add-membership', args=[club.id])
+        resp = self.client.post(url, payload)
+        self.assertEqual(resp.status_code, status.HTTP_403_FORBIDDEN)
+
+        self.client.login(username=self.u2.username, password='pass')
+        resp = self.client.post(url, payload)
+        self.assertEqual(resp.status_code, status.HTTP_302_FOUND)
+
+        self.assertEqual(NamedMembershipClub.objects.filter(club=club, student=self.u2.student).count(), 1)
 
