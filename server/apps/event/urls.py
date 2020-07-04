cb7def3aa49a181317afc5f6c21e05d796eca82e
Robin TROESCH <38283096+RobinetFox@users.noreply.github.com>
2020-07-04 00:14:32 -0700

    ajout de la possibilite de s'ajouter ou de se retirer d'un event

--- server/apps/event/urls.py
+++ server/apps/event/urls.py
@@ -7,4 +7,6 @@ app_name = 'event'
 
 urlpatterns = [
     path('event/<slug:event_slug>/', EventDetailView.as_view(), name='detail'),
+    path('event/<slug:event_slug>/participants/add', add_participant, name='add-participant'),
+    path('event/<slug:event_slug>/participants/delete', remove_participant, name='remove-participant')
 ]
