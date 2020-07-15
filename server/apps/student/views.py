9497869c1e8ec24feb49d7a846a3efe141c03ed4
Robin TROESCH <38283096+RobinetFox@users.noreply.github.com>
2020-07-15 12:17:08 -0700

    improve the basic update view of a student

--- server/apps/student/views.py
+++ server/apps/student/views.py
@@ -31,7 +31,7 @@ class StudentProfile(LoginRequiredMixin, DetailView):
 
 class StudentProfileEdit(UserPassesTestMixin, UpdateView):
     model = Student
-    fields = ['promo', 'picture']
+    fields = ['promo', 'picture', 'faculty', 'double_degree']
     template_name = 'student/update_profile.html'
     def test_func(self):
         self.object = self.get_object()
