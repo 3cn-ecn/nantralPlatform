b9ce0416122a0cbd901114996be1342bbae64584
Robin TROESCH <38283096+RobinetFox@users.noreply.github.com>
2020-08-07 11:27:44 -0700

    push

--- server/apps/academic/api_views.py
+++ server/apps/academic/api_views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics
 
 from .serializers import CourseSerializer
-from .models import Course
+from .models import Course, FollowCourse
 
 class CourseODList(generics.ListAPIView):
     queryset = Course.objects.filter(course_type='OD')
     serializer_class = CourseSerializer
+
+class CourseRegistration(generics.CreateAPIView):
