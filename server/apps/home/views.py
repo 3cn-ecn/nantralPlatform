c8cbddcb605630ebe62fa57db9cfccd85db524a2
Thibaut Berthomé <th.berthome@gmail.com>
2020-09-05 23:30:21 +0200

    modifs

--- server/apps/home/views.py
+++ server/apps/home/views.py
@@ -35,14 +35,14 @@ class SuggestionView(LoginRequiredAccessMixin, FormView):
 
 def event_sort(events):
     tri = {}
-    tri["Aujourd'hui"] = list()
-    tri["Demain"] = list()
     jours = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"]
     mois = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"]
     for event in events:
         if event.date.date() == date.today():
+            tri["Aujourd'hui"] = list()
             tri["Aujourd'hui"].append(event)
         elif event.date.date() == (date.today()+timedelta(days=1)):
+            tri["Demain"] = list()
             tri["Demain"].append(event)
         else:
             written_date = jours[event.date.weekday()] + " " + str(event.date.day) + " " +mois[event.date.month-1]
