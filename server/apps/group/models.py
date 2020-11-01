368d1ecb156537d9ba2b7b0c941a80c26b2cc7fd
Robin TROESCH <38283096+RobinetFox@users.noreply.github.com>
2020-11-01 19:51:41 +0100

    fix bug in Group get parent

--- server/apps/group/models.py
+++ server/apps/group/models.py
@@ -21,14 +21,21 @@ path_and_rename_club = PathAndRename("groups/logo/club")
 path_and_rename_liste = PathAndRename("groups/logo/liste")
 path_and_rename_group = PathAndRename("groups/logo/group")
 
+
 class Group(models.Model):
-    name = models.CharField(verbose_name='Nom du groupe', unique=True, max_length=200)
-    description = models.TextField(verbose_name='Description du groupe', blank=True)
-    admins = models.ManyToManyField(Student, verbose_name='Administrateur.rice.s du groupe', related_name='%(class)s_admins', blank=True)
-    members = models.ManyToManyField(Student, verbose_name='Membres du groupe', related_name='%(class)s_members')
-    logo = models.ImageField(verbose_name='Logo du groupe', blank=True, null=True, upload_to=path_and_rename_group)
+    name = models.CharField(verbose_name='Nom du groupe',
+                            unique=True, max_length=200)
+    description = models.TextField(
+        verbose_name='Description du groupe', blank=True)
+    admins = models.ManyToManyField(
+        Student, verbose_name='Administrateur.rice.s du groupe', related_name='%(class)s_admins', blank=True)
+    members = models.ManyToManyField(
+        Student, verbose_name='Membres du groupe', related_name='%(class)s_members')
+    logo = models.ImageField(verbose_name='Logo du groupe',
+                             blank=True, null=True, upload_to=path_and_rename_group)
     slug = models.SlugField(max_length=40, unique=True, blank=True)
     parent = models.SlugField(max_length=40, blank=True, null=True)
+
     class Meta:
         abstract = True
 
@@ -40,18 +47,20 @@ class Group(models.Model):
         if user.is_superuser or user.is_staff:
             return True
         student = Student.objects.filter(user=user).first()
-        return student in self.admins.all() or self.get_parent() is not None and self.get_parent().is_admin(user)
-    
+        return student in self.admins.all() or self.get_parent is not None and self.get_parent.is_admin(user)
+
     def is_member(self, user: User) -> bool:
         """Indicates if a user is member."""
         if not user.student:
             return False
         student = Student.objects.filter(user=user).first()
         return student in self.members.all()
-    
+
     @property
     def get_parent(self):
         """Get the parent group of this group."""
+        if self.parent == self.slug:
+            return None
         return Group.get_group_by_slug(self.parent)
 
     @staticmethod
@@ -64,17 +73,20 @@ class Group(models.Model):
             return Liste.objects.get(slug=slug)
         else:
             return None
-    
+
     @property
     def get_absolute_url(self):
         return reverse('group:detail', kwargs={'group_slug': self.slug})
 
-## Clubs
+# Clubs
+
 
 class Club(Group):
-    members =  models.ManyToManyField(Student, through='NamedMembershipClub')
-    bdx_type =  models.CharField(verbose_name='Type de club BDX', choices=TYPE_BDX, max_length=60)
-    logo = models.ImageField(verbose_name='Logo du club', blank=True, null=True, upload_to=path_and_rename_club)
+    members = models.ManyToManyField(Student, through='NamedMembershipClub')
+    bdx_type = models.CharField(
+        verbose_name='Type de club BDX', choices=TYPE_BDX, max_length=60)
+    logo = models.ImageField(verbose_name='Logo du club',
+                             blank=True, null=True, upload_to=path_and_rename_club)
 
     def save(self, *args, **kwargs):
         self.slug = f'club--{slugify(self.name)}'
@@ -91,14 +103,18 @@ class Club(Group):
 
 
 class NamedMembershipClub(models.Model):
-    function = models.CharField(verbose_name='Poste occupé', max_length=200, blank=True)
-    year = models.IntegerField(verbose_name='Année du poste', blank=True, null=True)
+    function = models.CharField(
+        verbose_name='Poste occupé', max_length=200, blank=True)
+    year = models.IntegerField(
+        verbose_name='Année du poste', blank=True, null=True)
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     club = models.ForeignKey(Club, on_delete=models.CASCADE)
-    class Meta:
-        unique_together = ('function', 'year','student', 'club')
 
-## Listes
+    class Meta:
+        unique_together = ('function', 'year', 'student', 'club')
+
+# Listes
+
 
 TYPE_LISTE = [
     ('BDA', 'Bureau des Arts'),
@@ -108,15 +124,18 @@ TYPE_LISTE = [
 
 
 class Liste(Group):
-    liste_type =  models.CharField(verbose_name='Type de liste BDX', choices=TYPE_LISTE, max_length=60)
-    year = models.IntegerField(verbose_name='Année de la liste', blank=True, null=True)
+    liste_type = models.CharField(
+        verbose_name='Type de liste BDX', choices=TYPE_LISTE, max_length=60)
+    year = models.IntegerField(
+        verbose_name='Année de la liste', blank=True, null=True)
     members = models.ManyToManyField(Student, through='NamedMembershipList')
-    logo = models.ImageField(verbose_name='Logo de la liste', blank=True, null=True, upload_to=path_and_rename_liste)
+    logo = models.ImageField(verbose_name='Logo de la liste',
+                             blank=True, null=True, upload_to=path_and_rename_liste)
 
     def save(self, *args, **kwargs):
         self.slug = f'liste--{slugify(self.name)}'
         super(Liste, self).save(*args, **kwargs)
-    
+
     def is_member(self, user: User) -> bool:
         """Indicates if a user is member."""
         if user.is_anonymous or not user.is_authenticated:
@@ -128,11 +147,14 @@ class Liste(Group):
 
 
 class NamedMembershipList(models.Model):
-    function = models.CharField(verbose_name='Poste occupé', max_length=200, blank=True)
+    function = models.CharField(
+        verbose_name='Poste occupé', max_length=200, blank=True)
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     liste = models.ForeignKey(Liste, on_delete=models.CASCADE)
+
     class Meta:
-        unique_together = ('function','student', 'liste')
+        unique_together = ('function', 'student', 'liste')
+
 
 @receiver(m2m_changed, sender=Group.admins.through)
 def admins_changed(sender, instance, action, pk_set, reverse, model, **kwargs):
@@ -144,7 +166,8 @@ def admins_changed(sender, instance, action, pk_set, reverse, model, **kwargs):
                 'group': instance,
                 'user': user
             })
-            user.email_user(f'Vous êtes admin de {instance}', mail, 'group-manager@nantral-platform.fr', html_message=mail)
+            user.email_user(f'Vous êtes admin de {instance}', mail,
+                            'group-manager@nantral-platform.fr', html_message=mail)
     elif action == "post_remove":
         for pk in pk_set:
             user = User.objects.get(pk=pk)
@@ -152,4 +175,5 @@ def admins_changed(sender, instance, action, pk_set, reverse, model, **kwargs):
                 'group': instance,
                 'user': user
             })
-            user.email_user(f'Vous n\'êtes plus admin de {instance}', mail, 'group-manager@nantral-platform.fr', html_message=mail)
+            user.email_user(
+                f'Vous n\'êtes plus admin de {instance}', mail, 'group-manager@nantral-platform.fr', html_message=mail)
