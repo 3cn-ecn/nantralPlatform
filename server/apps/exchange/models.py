51c89cb1597df151421b50a2ea98a58e83aa4b83
ThibautB <thibaut.berthome@mines-paristech.fr>
2021-01-21 11:55:40 +0100

    Correction sur l'affichage des options : limité aux options disciplinaires

--- server/apps/exchange/models.py
+++ server/apps/exchange/models.py
@@ -5,5 +5,5 @@ from apps.academic.models import Course
 
 class Exchange(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    got = models.ForeignKey(Course, related_name = 'got_by', on_delete=models.CASCADE)
-    wanted = models.ManyToManyField(Course, related_name = 'wanted_by')
+    got = models.ForeignKey(Course, related_name = 'got_by', on_delete=models.CASCADE, limit_choices_to={'course_type': 'OD'})
+    wanted = models.ManyToManyField(Course, related_name = 'wanted_by', limit_choices_to={'course_type': 'OD'})
