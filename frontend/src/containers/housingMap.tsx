d0dacf8f5a8c23e209164c2be9c2753e2ae6458d
Sarrus1 <omegasix0611@gmail.com>
2021-04-22 17:03:04 +0200

    Added a show my location feature

--- frontend/src/containers/housingMap.tsx
+++ frontend/src/containers/housingMap.tsx
@@ -1,7 +1,16 @@
 ﻿import * as React from "react";
 import { Component, PureComponent, useState, useEffect } from "react";
 import ReactDOM, { render } from "react-dom";
-import MapGL, { Marker } from "react-map-gl";
+import MapGL, { Marker, GeolocateControl} from "react-map-gl";
+
+
+const geolocateStyle = {
+  top: 0,
+  left: 0,
+  margin: 10
+};
+const positionOptions = {enableHighAccuracy: true};
+
 
 const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
   c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
@@ -63,6 +72,12 @@ function Root(props) {
       mapboxApiAccessToken={props.api_key}
     >
       {markers}
+      <GeolocateControl
+        style={geolocateStyle}
+        positionOptions={positionOptions}
+        trackUserLocation
+        auto
+      />
     </MapGL>
   );
 }
