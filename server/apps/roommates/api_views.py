93befa4826348b5b9bfe59a9005aa4782dc4864f
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2021-04-24 00:05:56 +0200

    Create api view to create Housing

--- server/apps/roommates/api_views.py
+++ server/apps/roommates/api_views.py
@@ -4,8 +4,15 @@ from rest_framework.response import Response
 from apps.roommates.serializers import HousingSerializer
 from apps.roommates.models import Housing
 
-class HousingView(generics.ListCreateAPIView):
+
+class CreateHousingView(generics.CreateAPIView):
     serializer_class = HousingSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class HousingView(generics.ListAPIView):
+    serializer_class = HousingSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
     def get_queryset(self):
         return Housing.objects.all()
--- server/apps/roommates/models.py
+++ server/apps/roommates/models.py
@@ -2,20 +2,21 @@ from django.db import models
 from apps.group.models import Group
 from django.utils.text import slugify
 
+
 class Housing(models.Model):
-	adress = models.CharField(max_length=250)
-	latitude = models.FloatField()
-	longitude = models.FloatField()
+    adress = models.CharField(max_length=250)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
 
 class Roommates(Group):
-	begin_date = models.DateField()
-	end_date = models.DateField()
-	housing = models.ForeignKey(to=Housing, on_delete=models.CASCADE)
+    begin_date = models.DateField()
+    end_date = models.DateField()
+    housing = models.ForeignKey(to=Housing, on_delete=models.CASCADE)
 
-	class Meta:
-		verbose_name_plural = "Roommates"
+    class Meta:
+        verbose_name_plural = "Roommates"
 
-	def save(self, *args, **kwargs):
-		self.slug = f'coloc--{slugify(self.name)}'
-		super(Roommates, self).save(*args, **kwargs)
-
+    def save(self, *args, **kwargs):
+        self.slug = f'coloc--{slugify(self.name)}'
+        super(Roommates, self).save(*args, **kwargs)
