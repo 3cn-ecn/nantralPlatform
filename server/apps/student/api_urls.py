1010ee322c018ab08bf662a2895acdab6562ebda
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2021-05-04 23:34:16 +0200

    api url for student

--- server/apps/student/api_urls.py
+++ server/apps/student/api_urls.py
@@ -6,6 +6,9 @@ from .api_views import *
 app_name = 'student'
 
 urlpatterns = [
-    path('<slug:student_id>/courses/', StudentCoursesView.as_view(), name='courses'),
-    path('<slug:student_id>/courses/<slug:pk>', StudentEditFollowCourse.as_view(), name='unfollow-course'),
+    path('<slug:student_id>/courses/',
+         StudentCoursesView.as_view(), name='courses'),
+    path('<slug:student_id>/courses/<slug:pk>',
+         StudentEditFollowCourse.as_view(), name='unfollow-course'),
+    path('', StudentList.as_view(), name='list')
 ]
