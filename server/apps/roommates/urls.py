f0173fa6c0d4e03b8b0b6427c010c320b23a9826
Bromorr <maxime.gour@gmail.com>
2021-05-05 22:10:57 +0200

    Début de la detail view des colocs
    Ajout d'un url basé sur l'id de housing. Ajout d'une vue et de son template.

--- server/apps/roommates/urls.py
+++ server/apps/roommates/urls.py
@@ -6,5 +6,6 @@ from .views import *
 app_name = 'roommates'
 
 urlpatterns = [
-    path('housing/', HousingMap.as_view(), name='housing-map')
+    path('housing/', HousingMap.as_view(), name='housing-map'),
+    path('detail/<int:housing_id>', HousingDetailView.as_view(), name='housing-view'), 
 ]
--- server/apps/roommates/views.py
+++ server/apps/roommates/views.py
@@ -2,7 +2,8 @@ from typing import Any, Dict
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
-from apps.roommates.models import Housing
+from apps.roommates.models import Housing, Roommates
+from django.views.generic.detail import DetailView #Ajouté par moi
 
 from django.conf import settings
 
@@ -13,3 +14,13 @@ class HousingMap(LoginRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         context['MAPBOX_API_KEY'] = settings.MAPBOX_API_KEY
         return context
+
+
+#Ajouté par moi
+class HousingDetailView(LoginRequiredMixin, TemplateView):
+    template_name = 'roommates/housing_detail.html'
+    model = Roommates
+
+    def get_context_data(self, **kwargs):
+        context= super().get_context_data(**kwargs)
+        context['Housing']= Housing.objects.filter(id = kwargs['housing_id']) #Je veux la coloc qui correspond aux colocataires dont je parle. Troooop de questions.
