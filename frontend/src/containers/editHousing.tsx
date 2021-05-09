e7489e642d2baf5178b60cc2d5c2acd2e30d1541
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2021-05-09 23:43:11 +0200

    format

--- frontend/src/containers/editHousing.tsx
+++ frontend/src/containers/editHousing.tsx
@@ -1,86 +1,191 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 import ReactDOM, { render } from "react-dom";
-import {Button, Card, Modal, Form} from 'react-bootstrap';
+import { Button, Card, Modal, Form } from "react-bootstrap";
 
-axios.defaults.xsrfCookieName = 'csrftoken'
-axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
-function EditHousing(props){
-    const [searchedStudents, updateSearchedStuents] = useState([]);
-    const [roommates, updateRoommates] = useState([]);
-    const [showModal, updateShowModal] = useState(false);
-    const [currentColocName, updateCurrentColocName] = useState(null);
-    const [currentColocStart, updateCurrentColocStart] = useState(null);
-    useEffect(() => {
-        getRoommates()
-    }, []);
-    function createRoommates(){
-        axios.post(props.roommates_api_url,{
-            'name': currentColocName,
-            'begin_date': currentColocStart
-        })
-        .then(() => {
-            getRoommates();
-            updateShowModal(!showModal);
-        })
-    }
-    function getStudents(search: string){
-        fetch(`${props.student_url}?search=${search}`)
-        .then((resp) => resp.json()
-        .then((students) => {
-            updateSearchedStuents(searchedStudents);
-        }))
-    }
-    function getRoommates(){
-        fetch(props.roommates_api_url)
-        .then((resp) => resp.json()
-        .then((roommatesGroups) => {
-            updateRoommates(roommatesGroups)
-        }))
-    }
-    return(
+function EditHousing(props) {
+  const [searchedStudents, updateSearchedStuents] = useState([]);
+  const [roommates, updateRoommates] = useState([]);
+  const [showModalRommatesGroupNew, updateshowModalRommatesGroupNew] = useState(
+    false
+  );
+  const [showModalRommatesMember, updateShowModalRommatesMember] = useState(
+    false
+  );
+  const [currentColocName, updateCurrentColocName] = useState(null);
+  const [currentColocStart, updateCurrentColocStart] = useState(null);
+  const [currentColocAddMe, updateCurrentColocAddMe] = useState(false);
+  const [currentColocNickName, updateCurrentColocNickName] = useState(null);
+  const [currentMembers, updateCurrentMembers] = useState([]);
+  useEffect(() => {
+    getRoommates();
+  }, []);
+  function createRoommates() {
+    axios
+      .post(props.roommates_api_url, {
+        name: currentColocName,
+        begin_date: currentColocStart,
+        add_me: currentColocAddMe,
+        nickname: currentColocNickName,
+      })
+      .then(() => {
+        getRoommates();
+        updateshowModalRommatesGroupNew(!showModalRommatesGroupNew);
+      });
+  }
+  function getStudents(search: string) {
+    fetch(`${props.student_url}?search=${search}`).then((resp) =>
+      resp.json().then((students) => {
+        updateSearchedStuents(searchedStudents);
+      })
+    );
+  }
+  function getRoommates() {
+    fetch(props.roommates_api_url).then((resp) =>
+      resp.json().then((roommatesGroups) => {
+        console.log(roommatesGroups);
+        updateRoommates(roommatesGroups);
+      })
+    );
+  }
+  return (
+    <div>
+      <h2>Qui est passé par là:</h2>
+      {roommates.length > 0 && (
         <div>
-            <h2>Qui est passé par là:</h2>
-            {roommates.length > 0 && <div>
-                {roommates.map(roommateGroup => (
-                    <Card>
-                        <Card.Header>{roommateGroup['name']}</Card.Header>
-                        <Card.Body>
-                            {roommateGroup['members'].map(member => <p>
-                                {member['first_name']} {member['last_name']} {member['last_name']} {member['nickname']}
-                            </p>)}
-                        </Card.Body>
-                    </Card>
+          {roommates.map((roommateGroup) => (
+            <Card>
+              <Card.Header>{roommateGroup["name"]}</Card.Header>
+              <Card.Body>
+                {roommateGroup["members"].map((member) => (
+                  <p>
+                    {member["student"]["first_name"]}{" "}
+                    {member["student"]["last_name"]}{" "}
+                    {member["student"]["last_name"]} alias {member["nickname"]}
+                  </p>
                 ))}
-                </div>}
-                <Button onClick={() => updateShowModal(!showModal)}>Ajouter une nouvelle coloc ?</Button>
-            <Modal show={showModal}>
-                <Form>
-
-
-                </Form>
-                <Modal.Footer>
-                    <Form>
-                        <Form.Group controlId="name">
-                            <Form.Label>Nom de votre coloc:</Form.Label>
-                            <Form.Control type="text" onInput={(event) => {updateCurrentColocName(event.target.value)}} placeholder="Le Terminal" />
-                        </Form.Group>
-                        <Form.Group controlId="name">
-                            <Form.Label>Date d'emenagement:</Form.Label>
-                            <Form.Control type="date" onInput={(event) => {updateCurrentColocStart(event.target.value)}} />
-                        </Form.Group>
-                    </Form>
-                    <Button onClick={() => updateShowModal(!showModal)} variant="secondary">Fermer</Button>
-                    <Button onClick={() => {
-                        createRoommates();
-                        updateShowModal(!showModal);
-                        }} variant="success">Enregistrer</Button>
-                </Modal.Footer>
-            </Modal>
+              </Card.Body>
+            </Card>
+          ))}
         </div>
-    )
+      )}
+      <Button
+        onClick={() =>
+          updateshowModalRommatesGroupNew(!showModalRommatesGroupNew)
+        }
+      >
+        Ajouter une nouvelle coloc ?
+      </Button>
+      <Modal show={showModalRommatesGroupNew}>
+        <Modal.Footer>
+          <Form>
+            <Form.Group controlId="name">
+              <Form.Label>Nom de votre coloc:</Form.Label>
+              <Form.Control
+                type="text"
+                onInput={(event) => {
+                  updateCurrentColocName(event.target.value);
+                }}
+                placeholder="Le Terminal"
+              />
+            </Form.Group>
+            <Form.Group controlId="begin_date">
+              <Form.Label>Date d'emenagement:</Form.Label>
+              <Form.Control
+                type="date"
+                onInput={(event) => {
+                  updateCurrentColocStart(event.target.value);
+                }}
+              />
+            </Form.Group>
+            <Form.Group controlId="add_me">
+              <Form.Check
+                type="checkbox"
+                onInput={(event) => {
+                  updateCurrentColocAddMe(event.target.value);
+                }}
+                label="M'ajouter dans la coloc"
+              />
+            </Form.Group>
+            {currentColocAddMe && (
+              <Form.Group controlId="nickname">
+                <Form.Label>Un surnom?</Form.Label>
+                <Form.Control
+                  type="text"
+                  onInput={(event) => {
+                    updateCurrentColocNickName(event.target.value);
+                  }}
+                  placeholder="Gordon"
+                />
+                <Form.Text className="text-muted">
+                  Si vous avez un surnom au sein de votre coloc que vous
+                  souhaitez afficher.
+                </Form.Text>
+              </Form.Group>
+            )}
+          </Form>
+          <Button
+            onClick={() => {
+              updateshowModalRommatesGroupNew(!showModalRommatesGroupNew);
+              updateCurrentColocAddMe(false);
+            }}
+            variant="secondary"
+          >
+            Fermer
+          </Button>
+          <Button
+            onClick={() => {
+              createRoommates();
+              updateshowModalRommatesGroupNew(!showModalRommatesGroupNew);
+              updateCurrentColocAddMe(false);
+            }}
+            variant="success"
+          >
+            Enregistrer
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={showModalRommatesMember}>
+        <Modal.Header>{currentColocName}</Modal.Header>
+        <Modal.Body>
+          {currentMembers.length > 0 &&
+            currentMembers.map((member) => (
+              <p>{member["student"]["first_name"]}</p>
+            ))}
+          <Form>
+            <Form.Group controlId="name">
+              <Form.Label>Ajouter quelqu'un:</Form.Label>
+              <Form.Control
+                type="text"
+                onInput={(event) => {
+                  getStudents(event.target.value);
+                }}
+                placeholder="Jean"
+              />
+              {searchedStudents.length > 0 &&
+                searchedStudents.map((suggestion) => (
+                  <div>
+                    <Button onClick={() => selectStudent(suggestion)}>
+                      {suggestion.first_name} {suggestion.last_name}
+                    </Button>
+                    <br />
+                  </div>
+                ))}
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
 }
 
-
-render(<EditHousing student_url={student_url} roommates_api_url={roommates_api_url}/>, document.getElementById("root"));
+render(
+  <EditHousing
+    student_url={student_url}
+    roommates_api_url={roommates_api_url}
+  />,
+  document.getElementById("root")
+);
