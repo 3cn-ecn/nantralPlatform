a2252109af60c83198d260861a4a9a6ff79ab22e
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2021-05-23 23:28:37 +0200

    Update production.py

--- server/config/settings/production.py
+++ server/config/settings/production.py
@@ -42,6 +42,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
+    'ACL': 'public-read'
 }
 AWS_S3_REGION_NAME = 'eu-west-3'
 
