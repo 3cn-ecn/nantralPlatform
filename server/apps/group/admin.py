97742e3da90f66af77de069fa2876c4bbe9f1944
hydrielax <alexidelage@gmail.com>
2021-06-10 18:09:17 +0200

    Change some stuffs for views and templates for bdx

--- server/apps/group/admin.py
+++ server/apps/group/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 
 class ClubAdmin(admin.ModelAdmin):
-    list_display = ['name', 'bdx_type']
+    list_display = ['name'] #, 'bdx_type']
 
 
 class ClubMembershipsAdmin(admin.ModelAdmin):
@@ -25,3 +25,5 @@ admin.site.register(Liste, ListeAdmin)
 admin.site.register(AdminRightsRequest)
 admin.site.register(ReseauSocial)
 admin.site.register(LienSocialClub)
+admin.site.register(BDX)
+admin.site.register(NamedMembershipBDX)
