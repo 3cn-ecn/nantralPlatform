f375f75c8bee991ac3f7a2d19f6b53b7bdcce6e0
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2021-06-13 01:06:13 +0200

    Update admin.py

--- server/apps/club/admin.py
+++ server/apps/club/admin.py
@@ -6,9 +6,14 @@ class ClubAdmin(admin.ModelAdmin):
     list_display = ['name', 'bdx_type']
 
 
+class BDXAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
 class ClubMembershipsAdmin(admin.ModelAdmin):
     list_display = ['student', 'club', 'function', 'year']
 
 
 admin.site.register(Club, ClubAdmin)
 admin.site.register(NamedMembershipClub, ClubMembershipsAdmin)
+admin.site.register(BDX, BDXAdmin)
