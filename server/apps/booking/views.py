6c33cf4f0f987d10c04cf532975b2a6c318413ff
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2021-07-06 21:16:05 +0200

    create view and template

--- server/apps/booking/views.py
+++ server/apps/booking/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, ListView
+from django.shortcuts import redirect
+from apps.utils.accessMixins import UserIsAdmin
+from .models import Service
 
-# Create your views here.
+from apps.group.models import Group
+
+
+class UpdateGroupListServicesView(UserIsAdmin, ListView):
+    model = Service
+    template_name = 'booking/services/update/list.html'
+
+    def get_queryset(self):
+        return Service.objects.filter(proposed_by=self.kwargs['group_slug'])
+
+
+class CreateServiceView(UserIsAdmin, CreateView):
+    model = Service
+    fields = ['name', 'description', 'conditions', 'price', 'paiement_link']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['object'] = Group.get_group_by_slug(self.kwargs['group_slug'])
+        return context
+
+
+class UpdateServiceView(UserIsAdmin, UpdateView):
+    model = Service
+    fields = ['name', 'description', 'conditions', 'price', 'paiement_link']
