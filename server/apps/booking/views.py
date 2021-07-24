8b0634f7df55dc9bb470a9faf1cc6c9c6215dfc9
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2021-07-24 22:20:24 +0200

    create list view for services

--- server/apps/booking/views.py
+++ server/apps/booking/views.py
@@ -9,10 +9,15 @@ from apps.group.views import GroupSlugFonctions
 
 class UpdateGroupListServicesView(GroupSlugFonctions, UserIsAdmin, ListView):
     model = Service
-    template_name = 'booking/services/update/list.html'
+    template_name = 'booking/update/service/list.html'
 
     def get_queryset(self):
         return Service.objects.filter(proposed_by=self.get_slug)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['object'] = Group.get_group_by_slug(self.get_slug)
+        return context
 
 
 class CreateServiceView(GroupSlugFonctions, UserIsAdmin, CreateView):
