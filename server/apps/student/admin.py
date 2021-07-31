4db7050178c7ef0de5d6d637c1cd63e9593239a2
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2021-07-31 13:53:14 +0200

    Update admin.py

--- server/apps/student/admin.py
+++ server/apps/student/admin.py
@@ -21,7 +21,8 @@ class StudentAdmin(admin.ModelAdmin):
     def metrics_view(self, request):
         context = dict(
             self.admin_site.each_context(request=request),
-            promos=Student.objects.all().values('promo').annotate(count=Count('promo')),
+            promos=Student.objects.all().values('promo').annotate(
+                count=Count('promo')).order_by(),
             nb_students=Student.objects.all().count()
         )
         return TemplateResponse(request=request, template='admin/student/metrics.html', context=context)
