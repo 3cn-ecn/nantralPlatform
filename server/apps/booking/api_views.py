db51c0087411ca5a2cf573043077df7dc03d7c67
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2021-07-31 19:39:51 +0200

    booking api view

---
+++ server/apps/booking/api_views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics, permissions
+from .serializers import AvailabilitySerializer
+from .models import Availabilty
+
+
+class ListCreateAvailaibilites(generics.ListCreateAPIView):
+    """Lists or Create availabilities for a given service."""
+    serializer_class = AvailabilitySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Availabilty.objects.filter(service=self.kwargs['id'])
