8dd054e7a571601c12dee504e37a65ddf18b4219
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2021-08-05 22:19:02 +0200

    add is super admin access mixin

--- server/apps/utils/accessMixins.py
+++ server/apps/utils/accessMixins.py
@@ -4,6 +4,7 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 
 from apps.group.models import Group
 
+
 class LoginRequiredAccessMixin(object):
 
     @method_decorator(login_required)
@@ -16,4 +17,11 @@ class UserIsAdmin(UserPassesTestMixin):
         if self.request.user.is_authenticated:
             group = Group.get_group_by_slug(self.get_slug)
             return group.is_admin(self.request.user)
+        return False
+
+
+class UserIsSuperAdmin(UserPassesTestMixin):
+    def test_func(self):
+        if self.request.user.is_authenticated:
+            return self.request.user.is_superuser
         return False
