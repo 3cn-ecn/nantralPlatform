5ef05a8af812dea3cfed2b1f93eb69ba14148aa5
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2021-08-20 19:41:19 +0200

    fix temp access request

--- server/apps/club/migrations/0015_test_clubs_debug.py
+++ server/apps/club/migrations/0015_test_clubs_debug.py
@@ -1,13 +1,16 @@
 # Test clubs will be created for test instances.
+import os
 from django.conf import settings
 from django.db import migrations
 from apps.club.models import Club, BDX
 
 
 def create_clubs(apps, schema_editor):
-    bde = BDX.objects.create(
+    bde = BDX(
         name='BDE'
     )
+    bde.save()
+    bde.refresh_from_db()
     Club.objects.create(
         name='TestClubBDE',
         bdx_type=bde
@@ -15,10 +18,16 @@ def create_clubs(apps, schema_editor):
 
 
 if settings.DEBUG:
+    migrations_files = os.listdir('apps/club/migrations')
+    migrations_files.sort()
+    migrations_files = [
+        migrations_file for migrations_file in migrations_files if migrations_file[0:3].isnumeric()]
+    latest_migration_file = migrations_files[-1][:-3]
+
     class Migration(migrations.Migration):
 
         dependencies = [
-            ('club', '0014_auto_20210806_1138'),
+            ('club', latest_migration_file),
         ]
 
         operations = [
