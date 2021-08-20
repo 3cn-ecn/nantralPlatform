7ec878748fd6b57a250b3b0eb735074191ca1be5
hydrielax <alexidelage@gmail.com>
2021-08-20 18:18:00 +0200

    Hide "Parrainage link" for 1A

--- server/apps/home/context_processors.py
+++ server/apps/home/context_processors.py
@@ -1,8 +1,11 @@
 # context processor for objects and 
 # variables needed in the navbar
 
+from datetime import date
+
 from apps.club.models import BDX
 from apps.family.models import Affichage
+
 
 def navbar_context(request):
 	bdx = BDX.objects.all()
@@ -11,4 +14,9 @@ def navbar_context(request):
 	except Exception:
 		Affichage().save()
 		phase = Affichage.objects.first().phase
-	return {'navbar_bdx': bdx, 'navbar_family_phase': phase}
+	try:
+		is_2A = request.user.student.promo < date.today().year
+	except Exception:
+		is_2A = False
+	show = phase > 0 and (is_2A or date.today() > date(date.today().year,8,30))
+	return {'navbar_bdx': bdx, 'navbar_family_show': show}
