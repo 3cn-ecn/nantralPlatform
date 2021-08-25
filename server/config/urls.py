eed96c84533e9503b8705137f64adb7017983b71
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2021-08-25 15:33:50 +0200

    Feat/caching (#447)
    * Install the debug toolbar
    
    * Enable caching
    
    * install memcached
    
    * add missing env vars
    
    * better settings for caching
    
    * cache bdx
    
    * force DEBUG toolbar to show

--- server/config/urls.py
+++ server/config/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 # pour importer les fichiers en dev local
@@ -20,6 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('__debug__/', include(debug_toolbar.urls)),
     path('admin/', admin.site.urls),
 
     path('account/', include('apps.account.urls', namespace='account')),
