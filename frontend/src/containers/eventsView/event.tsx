4227193cae7e2dd4dca74067b98c0c4807e0ee22
charles-zablit <zablitcharles@gmail.com>
2021-08-31 18:50:11 +0200

    Events' code refactoring

---
+++ frontend/src/containers/eventsView/event.tsx
@@ -0,0 +1,50 @@
+﻿import * as React from "react";
+import { EventInfos } from "./interfaces";
+import Truncate from "react-truncate";
+import { cardStyle, eventLink } from "./styles";
+import { ParticipateButton } from "./participateButton";
+
+var dayjs = require("dayjs");
+var isToday = require("dayjs/plugin/isToday");
+dayjs.extend(isToday);
+var isTomorrow = require("dayjs/plugin/isTomorrow");
+dayjs.extend(isTomorrow);
+require("dayjs/locale/fr");
+dayjs.locale("fr");
+
+export function Event(props): JSX.Element {
+  const urls = props.urls;
+  const eventInfos: EventInfos = props.eventInfos;
+
+  return (
+    <div className={`card pt-0 bg-${eventInfos.color}`} style={cardStyle}>
+      <div className="card-body">
+        <a
+          href={eventInfos.get_absolute_url}
+          style={eventLink}
+          className="mb-1"
+        >
+          {" "}
+          <h5 className="card-title">
+            {eventInfos.title} • Début :{" "}
+            {dayjs(eventInfos.date).format("HH:mm")} • {eventInfos.location}
+          </h5>
+          <h6 className="card-subtitle mb-2">{eventInfos.get_group_name}</h6>
+        </a>
+        <h6 className="card-subtitle mb-2">
+          <ParticipateButton
+            number_of_participants={eventInfos.number_of_participants}
+            urls={urls}
+            eventInfos={eventInfos}
+          />
+        </h6>
+        <Truncate lines={3}>
+          <p
+            className="card-text"
+            dangerouslySetInnerHTML={{ __html: eventInfos.description }}
+          ></p>
+        </Truncate>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/containers/eventsView/styles.ts
@@ -0,0 +1,21 @@
+﻿import { CSSProperties } from "react";
+
+export const spinnerDivStyle: CSSProperties = {
+  display: "flex",
+  justifyContent: "center",
+  alignContent: "center",
+  fontSize: "5rem",
+};
+
+export const spinnerStyle: CSSProperties = {
+  width: 75,
+  height: 75,
+};
+
+export const eventLink: CSSProperties = {
+  color: "white",
+};
+
+export const cardStyle: CSSProperties = {
+  boxShadow: "1px 1px 7px #999",
+};
