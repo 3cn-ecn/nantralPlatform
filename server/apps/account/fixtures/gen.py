0852b57eb1190669b3d7643fa32dbd76c85adf0e
charles-zablit <zablitcharles@gmail.com>
2021-08-31 17:21:48 +0200

    Add dummy users

---
+++ server/apps/account/fixtures/gen.py
@@ -0,0 +1,31 @@
+﻿import json
+import copy
+import names
+from django.conf import settings
+settings.configure()
+studentTemp = {
+    "is_superuser": False,
+    "is_staff": False,
+    "is_active": True,
+}
+
+L = []
+for i in range(1, 200):
+    newStudent = copy.deepcopy(studentTemp)
+    if i != 1:
+        firstName = names.get_first_name().lower()
+        lastName = names.get_last_name().lower()
+        newStudent["email"] = f'{firstName}.{lastName}@ec-nantes.fr'
+    else:
+        firstName = "robin"
+        lastName = "test"
+        newStudent["is_superuser"] = True
+        newStudent["is_staff"] = True
+        newStudent["email"] = 'robin@ec-nantes.fr'
+    newStudent["username"] = f'{firstName}{lastName}-{i}'
+    newStudent["first_name"] = f'{firstName}'
+    newStudent["last_name"] = f'{lastName}'
+
+    L.append(newStudent)
+with open('./apps/account/fixtures/data.json', 'w') as outfile:
+    json.dump(L, outfile)
--- server/apps/account/migrations/0001_create_test_account_dev.py
+++ server/apps/account/migrations/0001_create_test_account_dev.py
@@ -1,28 +1,26 @@
 from django.contrib.auth.hashers import make_password
 from django.db import migrations
 from django.conf import settings
+from apps.account.fixtures.fixture import dummyUsers
+from random import randint
 
 
 def create_user(apps, schema_editor):
     User = apps.get_registered_model('auth', 'User')
-    user = User(
-        username='user',
-        first_name='Robin',
-        last_name='Test',
-        email='robin@ec-nantes.fr',
-        password=make_password('pass'),
-        is_superuser=True,
-        is_staff=True,
-        is_active=True
-    )
-    user.save()
     Student = apps.get_registered_model('student', 'Student')
-    student = Student.objects.create(user=user)
-    student.save()
-    user.student = student
-    user.student.promo = 2017
-    user.student.save()
-    user.save()
+    password = make_password("pass")
+    for user in dummyUsers:
+        user = User(
+            password=password,
+            **user
+        )
+        user.save()
+        student = Student.objects.create(user=user)
+        student.save()
+        user.student = student
+        user.student.promo = randint(2017, 2021)
+        user.student.save()
+        user.save()
 
 
 if settings.DEBUG:
