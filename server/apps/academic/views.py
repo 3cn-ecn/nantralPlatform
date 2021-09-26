cdf4818fc4f59168ba557549c0d0f55554d5c282
Alexis Delage <alexis.delage@eleves.ec-nantes.fr>
2021-09-26 20:56:52 +0200

    I am an idiot

--- server/apps/academic/views.py
+++ server/apps/academic/views.py
@@ -14,7 +14,7 @@ class CoursesList(LoginRequiredMixin, ListView):
 
 class DetailCourseView(DetailGroupView):
     template_name = 'academic/detail.html'
-
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         group = context['object']
@@ -23,3 +23,5 @@ class DetailCourseView(DetailGroupView):
             this_year -= 1
         context['members'] = group.members.through.objects.filter(
             group=group, year=this_year).order_by('student__user__first_name')
+        return context
+    
