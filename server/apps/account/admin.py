ee6c6545de1f43d838bff033ed62a6ad70207fdb
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2021-09-29 21:40:45 +0200

    add reminder feature

--- server/apps/account/admin.py
+++ server/apps/account/admin.py
@@ -1,9 +1,43 @@
 from django.contrib import admin
+from django.contrib.sites.shortcuts import get_current_site
+from django.core import mail
+from django.template.loader import render_to_string
 from .models import TemporaryAccessRequest
-
+from django.conf import settings
 # Register your models here.
+
+
 class UserAdmin(admin.ModelAdmin):
     pass
 
 
-admin.site.register(TemporaryAccessRequest)
+class TemporaryAccessRequestAdmin(admin.ModelAdmin):
+    actions = ["send_reminder"]
+
+    @admin.action(description="Send reminder to upgrade account.")
+    def send_reminder(self, request, queryset):
+        connection = mail.get_connection()
+        current_site = get_current_site(request)
+        mails = []
+        for tempAccessReq in queryset:
+            tempAccessReq: TemporaryAccessRequest
+            email_html = render_to_string(
+                'account/mail/reminder_upgrade.html',
+                context={
+                    'tempAccess': tempAccessReq.user,
+                    'deadline': settings.TEMPORARY_ACCOUNTS_DATE_LIMIT,
+                    'domain': current_site.domain
+                }
+            )
+            email = mail.EmailMultiAlternatives(
+                subject="[Nantral-Platform] Rappel votre accès temporaire va expirer!",
+                body=email_html,
+                from_email='accounts@nantral-platform.fr',
+                to=[tempAccessReq.user.email]
+            )
+            email.attach_alternative(content=email_html, mimetype="text/html")
+            mails.append(email)
+        connection.send_messages(mails)
+
+
+admin.site.register(TemporaryAccessRequest, TemporaryAccessRequestAdmin)
