2d9a963f1aec71da60df9621b3a7e6287cb8b258
charles-zablit <zablitcharles@gmail.com>
2021-10-04 22:58:49 +0200

    Add club members API endpoint

--- server/apps/club/api_urls.py
+++ server/apps/club/api_urls.py
@@ -7,4 +7,6 @@ app_name = 'event'
 urlpatterns = [
     path('my-clubs', ListMyClubAPIView.as_view(),
          name='list-my-clubs'),
+    path('club-members', ListClubMembersAPIView.as_view(),
+         name='list-club-members'),
 ]
--- server/apps/club/api_views.py
+++ server/apps/club/api_views.py
@@ -1,7 +1,12 @@
 from rest_framework import generics, permissions
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
+
+from django.utils import timezone
 
 from .models import Club
 from .serializers import *
+from apps.student.serializers import StudentSerializer
 
 
 class ListMyClubAPIView(generics.ListAPIView):
@@ -14,3 +19,21 @@ class ListMyClubAPIView(generics.ListAPIView):
             allMembersClub = Club.objects.filter(
                 members__user=self.request.user)
             return allMembersClub
+
+
+class ListClubMembersAPIView(generics.ListAPIView):
+    """List all the members of a club."""
+    serializer_class = StudentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        clubSlug = self.request.query_params.get('slug')
+        club = get_object_or_404(Club, slug=clubSlug)
+        date_end = timezone.make_aware(timezone.now().today())
+        namedMemberships = club.members.through.objects.filter(
+            Q(group=club) & (Q(date_end__isnull=True) | Q(date_end__gt=date_end))
+        ).order_by('student__user__first_name')
+        allMembers = []
+        for membership in namedMemberships:
+            allMembers.append(membership.student)
+        return allMembers
