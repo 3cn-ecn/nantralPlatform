d468c5ec0f6b568c16959f77d4c643f88f54ed86
hydrielax <alexidelage@gmail.com>
2021-10-04 23:11:49 +0200

    Update 0005_migrate_data.py

--- server/apps/academic/migrations/0005_migrate_data.py
+++ server/apps/academic/migrations/0005_migrate_data.py
@@ -4,29 +4,23 @@ from django.db import migrations
 
 
 def migrate_data(apps, schema_editor):
-    # add slugs
-    try:
-        from apps.academic.models import Course
-        for course in Course.objects.all():
-            course.save()
-        # transfer memberships
-        from apps.academic.models import FollowCourse, NamedMembershipCourse
-        for follow in FollowCourse.objects.all():
-            year = follow.student.promo
-            if follow.when == 'EI2':
-                year += 1
-            if follow.when == 'EI3':
-                year += 2
-            if follow.when == 'M2':
-                year += 1
-            NamedMembershipCourse.objects.create(
-                group=follow.course,
-                student=follow.student,
-                year=year,
-            )
-    except Exception as err:
-        print(err)
-        print("échec de la migration 5 de academic")
+    # create slugs
+    Course = apps.get_model('academic', 'Course')
+    for course in Course.objects.all():
+        course.save()
+    # transfer memberships
+    FollowCourse = apps.get_model('academic', 'FollowCourse')
+    NamedMembershipCourse = apps.get_model('academic', 'NamedMembershipCourse')
+    for follow in FollowCourse.objects.all():
+        year = follow.student.promo
+        if follow.when == 'EI2': year +=1
+        if follow.when == 'EI3': year +=2
+        if follow.when == 'M2': year +=1
+        NamedMembershipCourse.objects.create(
+            group = follow.course,
+            student = follow.student,
+            year = year,
+        )
 
 
 class Migration(migrations.Migration):
