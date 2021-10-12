855aebdd503cd840f52ff4669d833805ada2d1e2
charles-zablit <zablitcharles@gmail.com>
2021-10-12 13:37:43 +0200

    Add colors and verbose output

--- server/install.sh
+++ server/install.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
 
-echo "========= Installing virtualenv"
+
+echo -e "${YELLOW}======== ${ORANGE}Checking on Python${NC}"
+{
+  command -v python3 >/dev/null 2>&1 &&
+  echo -e ${GREEN}Python 3 is installed
+
+} || {
+  echo -e ${RED}[ERROR]${ORANGE}Python 3 is not installed, check https://wiki.nantral-platform.fr to see how to install it.
+  exit 1;
+}
+echo -e "${YELLOW}========= ${GREEN}DONE ${YELLOW}========${NC}"
+
+echo -e "${YELLOW}========= ${ORANGE}Installing virtualenv${NC}"
 pip install virtualenv
-echo "========= DONE ========"
+echo -e "${YELLOW}========= ${GREEN}DONE ${YELLOW}========${NC}"
 
-echo "========= Creating environment"
+echo -e "${YELLOW}========= ${ORANGE}Creating environment${NC}"
 python3 -m virtualenv env
-echo "========= DONE ========="
+echo -e "${YELLOW}========= ${GREEN}DONE ${YELLOW}=========${NC}"
 
 . env/bin/activate
 
-echo "========= Installing requirements"
-pip install -r requirements.txt 2>/dev/null
-echo "========= DONE ========="
+echo -e "${YELLOW}========= ${ORANGE}Installing requirements${NC}"
+pip install -r requirements.txt
+echo -e "${YELLOW}========= ${GREEN}DONE ${YELLOW}=========${NC}"
 
-echo "========= Initializing database"
-python manage.py migrate 2>/dev/null
-echo "========= DONE ========="
+echo -e "${YELLOW}========= ${ORANGE}Initializing database${NC}"
+python manage.py migrate
+echo -e "${YELLOW}========= ${GREEN}DONE ${YELLOW}=========${NC}"
