510e4b2fc1e3326bae2cf73a818a5c8936d41d9f
charles-zablit <zablitcharles@gmail.com>
2021-10-12 18:46:12 +0200

    Split housingMap code

---
+++ frontend/src/containers/housingMap/clusterMarker.tsx
@@ -0,0 +1,17 @@
+﻿import * as React from "react";
+
+import "react-bootstrap-typeahead/css/Typeahead.css";
+
+import { clusterMarkerStyle, clusterMarkerContainerStyle } from "./styles";
+
+export function ClusterMarker(props): JSX.Element {
+  const { cluster, onClick } = props;
+
+  return (
+    <div style={clusterMarkerContainerStyle} onClick={onClick}>
+      <div style={clusterMarkerStyle} onClick={onClick}>
+        {cluster.properties.point_count}
+      </div>
+    </div>
+  );
+}
