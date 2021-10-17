59c8127b484f2c4fd582d530dd0c13c5449540f3
hydrielax <alexidelage@gmail.com>
2021-10-17 18:47:03 +0200

    Upgrade styles

--- frontend/src/containers/eventsView/event.tsx
+++ frontend/src/containers/eventsView/event.tsx
@@ -1,9 +1,9 @@
 ﻿import * as React from "react";
 import { EventInfos } from "./interfaces";
 import Truncate from "react-truncate";
-import { cardStyle } from "./styles";
 import { ParticipateButton } from "./participateButton";
 import { Card } from "react-bootstrap";
+import { getDate } from "./utils";
 
 var dayjs = require("dayjs");
 var isToday = require("dayjs/plugin/isToday");
@@ -18,13 +18,14 @@ export function Event(props): JSX.Element {
   const eventInfos: EventInfos = props.eventInfos;
 
   return (
-    <Card border={eventInfos.color} style={cardStyle}>
+    <Card border={eventInfos.color}>
       <Card.Img variant="top" src={eventInfos.image} />
       <Card.Body>
         <a
           href={eventInfos.get_absolute_url}
           className="mb-1 text-dark"
         >
+          <small className="text-uppercase">{getDate(eventInfos.date)}</small>
           <Card.Title>
             {eventInfos.title}
           </Card.Title>
