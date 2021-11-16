73535a424550f5a2d51ffd2630f620dda0e1eb8c
charles-zablit <zablitcharles@gmail.com>
2021-11-16 18:43:29 +0100

    Breaking: Bump PGSQL version

--- scripts/db_staging.py
+++ scripts/db_staging.py
@@ -1,4 +1,5 @@
 import psycopg2
+import docker
 from environs import Env
 import logging
 
@@ -10,10 +11,12 @@ logging.basicConfig(filename='db_staging.log',
 
 try:
     env = Env()
-    env.read_env("../deployment/.env")
+    env.read_env("../../nantralPlatform/deployment/.env")
     DB_USER = env.str("POSTGRES_USER")
     DB_PASSWORD = env.str("POSTGRES_PASSWORD")
     DB_NAME_STAGING = env.str("DB_NAME_STAGING")
+    DB_HOST = env.str("DB_HOSTNAME")
+    DB_PORT = env.str("DB_PORT")
     DB_NAME_PROD = env.str("DB_NAME")
     DB_CONTAINER = env.str("DB_CONTAINER")
     BUCKET = env.str("S3_BUCKET")
@@ -21,15 +24,17 @@ try:
     AWS_SECRET_ACCESS_KEY = env.str("AWS_SECRET_ACCESS_KEY")
     AWS_REGION = env.str("AWS_SES_REGION")
     ERROR_RECIPIENT = env.str("ERROR_RECIPIENT")
-
+    client = docker.from_env()
+    db_container = client.containers.get(DB_CONTAINER)
+    IP = db_container.attrs["NetworkSettings"]["Networks"]["deployment_default"]["IPAddress"]
     ps_connection = psycopg2.connect(user=DB_USER,
                                      password=DB_PASSWORD,
-                                     host="127.0.0.1",
-                                     port="5432")
-
+                                     host=IP,
+                                     port=DB_PORT)
+    ps_connection.autocommit = True
     cursor = ps_connection.cursor()
 
-    cursor.execute(f"DROP DATABASE IF EXISTS {DB_NAME_STAGING};")
+    cursor.execute(f"DROP DATABASE {DB_NAME_STAGING} WITH ( FORCE );")
     cursor.execute(
         f"CREATE DATABASE {DB_NAME_STAGING} WITH TEMPLATE {DB_NAME_PROD}")
 
