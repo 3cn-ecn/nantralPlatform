b03ecf148fced5b96cb4d92b980f0a6b5bb63b7d
hydrielax <alexidelage@gmail.com>
2021-12-16 15:15:31 +0100

    Update tests.py

--- server/apps/notification/tests.py
+++ server/apps/notification/tests.py
@@ -1,3 +1,18 @@
+from datetime import date
 from django.test import TestCase
+from apps.utils.utest import TestMixin
+from django.urls import reverse
+from rest_framework import status
 
-# Create your tests here.
+from .models import Subscription, Notification, SentNotification
+
+
+class TestNotification(TestCase, TestMixin):
+    def setUp(self):
+        self.user_setup()
+
+    def test_subscription(self):
+        Subscription.objects.create(
+            student=self.u2, page='club--bde')
+        self.assertTrue(Subscription.hasSubscribed('club--bde', self.u2))
+        self.assertFalse(Subscription.hasSubscribed('club--bde', self.u1))
