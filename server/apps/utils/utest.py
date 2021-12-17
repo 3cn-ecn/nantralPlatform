1ce5ef7bdf4982fbf68f7bbba0606d05a6b41027
hydrielax <alexidelage@gmail.com>
2021-12-17 11:57:25 +0100

    Better documentation

--- server/apps/utils/utest.py
+++ server/apps/utils/utest.py
@@ -4,6 +4,8 @@ User = get_user_model()
 
 class TestMixin(object):
     def user_setup(self):
+        """Create 3 standards users: u1, u2 and u3. Only u2 and u3
+        are linked to a Student instance"""
         self.u1 = User.objects.create_superuser(
             username='admin', email='admin@ec-nantes.fr', password='pass'
         )
@@ -11,10 +13,11 @@ class TestMixin(object):
         self.u3 = self.create_user('user3', 'user3@ec-nantes.fr')
     
     def create_user(self, username, email, is_active=True, name=""):
-        """Create a new User and a Student with it"""
+        """Create a new user AND A STUDENT with it"""
         u = User.objects.create(username=username, email=email)
         u.set_password('pass')
         u.is_active = is_active
+        u.name = name
         u.save()
         return u
     
