2e26de2853cb8dd3a21dfb0a6bc0bdf57907170b
IstyarConstantine <berylcassel@gmail.com>
2021-12-21 11:54:18 +0100

    Update Service Worker

--- server/static/app/app.js
+++ server/static/app/app.js
@@ -1,10 +1,18 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/static/app/sw.js').then(function(reg) {
-      // registration worked
-      console.log('Registration succeeded. Scope is ' + reg.scope);
-    }).catch(function(error) {
-      // registration failed
-      console.log('Registration failed with ' + error);
-    });
-  };
+  navigator.serviceWorker.register('/static/app/sw.js') 
+};
 
+// Show Notification
+function showNotification(){
+  Notification.requestPermission((result)=> {
+    if (result === 'granted') {
+      navigator.serviceWorker.ready.then((registration) => {
+        registration.showNotification('test',{
+          body: 'ceci est un test'
+        })
+      })
+    }   
+  })
+};
+
+showNotification();
--- server/static/app/sw.js
+++ server/static/app/sw.js
@@ -1,22 +1,8 @@
 // Installing Service Worker
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-    e.waitUntil((async () => {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
-  });
-
-// Show Notification
-function showNotification(){
-  Notification.requestPermission((result)=> {
-    if (result === 'granted') {
-      navigator.serviceWorker.ready.then((registration) => {
-        registration.showNotification('test',{
-          body: 'ceci est un test'
-        })
-      })
-    }   
-  })
-}
+this.addEventListener('install',function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.add('/static/');
+    })
+  );
+});
