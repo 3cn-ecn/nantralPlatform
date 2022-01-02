c5483af1a9a6754603062ef96045124ef07cef4e
hydrielax <alexidelage@gmail.com>
2022-01-02 11:01:57 +0100

    Readd the back button

--- frontend/src/containers/goBackButton.tsx
+++ frontend/src/containers/goBackButton.tsx
@@ -5,8 +5,8 @@ import ReactDOM, { render } from "react-dom";
 function GoBackMain(): JSX.Element {
   if (window.matchMedia('(display-mode: standalone)').matches) {
     return (
-			<div id="goBackButton" className="d-inline-block pe-3" onClick={(e)=>window.history.back()}>
-        <i aria-hidden="true" className="fas fa-chevron-left"></i>
+			<div className="d-inline-block pe-2" onClick={(e)=>window.history.back()}>
+        <i aria-hidden="true" className="fas fa-arrow-left"></i>
       </div>
     );
   }
