892dd74ab463a354d915aa96793f091779152a06
hydrielax <alexidelage@gmail.com>
2022-01-10 21:27:19 +0100

    Clean and improve toolbar

--- server/config/settings/base.py
+++ server/config/settings/base.py
@@ -174,51 +174,16 @@ customColorPalette = [
 
 CKEDITOR_5_CONFIGS = {
     'default': {
-        'toolbar': ['heading', '|', 'bold', 'italic', 'link',
-                    'bulletedList', 'numberedList', 'blockQuote', ],
-
-    },
-    'extends': {
-        'blockToolbar': [
-            'paragraph', 'heading1', 'heading2', 'heading3',
-            '|',
-            'bulletedList', 'numberedList',
-            '|',
-            'blockQuote', 'imageUpload'
-        ],
         'toolbar': [
-            'heading', '|', 'outdent', 'indent', '|',
-            'bold', 'italic', 'link', 'underline', 'strikethrough', 'code',
-            'subscript', 'superscript', 'highlight', '|',
-            'codeBlock', 'bulletedList', 'numberedList', 'todoList', '|',
-            'blockQuote', 'imageUpload', '|',
-            'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor',
-            'mediaEmbed', 'removeFormat', 'insertTable',
+            'heading', '|', '|',
+            'bold', 'italic', 'underline', 'strikethrough', 
+            'subscript', 'superscript', 'fontColor', 'fontBackgroundColor', 'removeFormat', '|',
+            'alignment', 'bulletedList', 'numberedList', '|',
+            'code', 'blockQuote', '|',
+            'link', 'imageUpload', 'mediaEmbed', 'insertTable', '|',
+            'sourceEditing'
         ],
-        'table': {
-            'contentToolbar': ['tableColumn', 'tableRow', 'mergeTableCells',
-                               'tableProperties', 'tableCellProperties'],
-            'tableProperties': {
-                'borderColors': customColorPalette,
-                'backgroundColors': customColorPalette
-            },
-            'tableCellProperties': {
-                'borderColors': customColorPalette,
-                'backgroundColors': customColorPalette
-            }
-        },
-        'heading': {
-            'options': [
-                   {'model': 'paragraph', 'title': 'Paragraph',
-                    'class': 'ck-heading_paragraph'},
-                   {'model': 'heading1', 'view': 'h1',
-                       'title': 'Heading 1', 'class': 'ck-heading_heading1'},
-                   {'model': 'heading2', 'view': 'h2',
-                       'title': 'Heading 2', 'class': 'ck-heading_heading2'},
-                   {'model': 'heading3', 'view': 'h3',
-                       'title': 'Heading 3', 'class': 'ck-heading_heading3'}
-            ]
-        }
+        'language': 'fr'
     }
 }
 
