3f5cb526ee52c0c391753970f1071ac15b719f3e
hydrielax <alexidelage@gmail.com>
2022-02-27 18:30:42 +0100

    Settings page

--- server/apps/notification/views.py
+++ server/apps/notification/views.py
@@ -1,35 +1,44 @@
 from django.views.generic import FormView, View, TemplateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
 # Create your views here.
 from apps.utils.slug import SLUG_GROUPS, get_model
 from .models import Subscription
+from .forms import SubscriptionForm
 
 
 class SettingsView(LoginRequiredMixin, TemplateView):
     template_name = "notification/settings.html"
 
+    def get_pages(self):
+        student = self.request.user.student
+        listOfSub = Subscription.objects.filter(student=student).values('page')
+        pages = [s['page'] for s in listOfSub]
+        return pages
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        student = self.request.user.student
         context['ariane'] = [
             {
                 'target': '#',
                 'label': 'Notifications'
             }
         ]
-        context['form'] = []
-        for app in SLUG_GROUPS.keys():
-            GroupModel = get_model(app)
-            liste = []
-            for group in GroupModel.objects.all():
-                liste.append({
-                    'name': group.name,
-                    'slug': group.full_slug,
-                    'subscribed': Subscription.hasSubscribed(group.full_slug, student)
-                })
-            context['form'].append({
-                'app': GroupModel.app,
-                'liste': liste
-            })
+        pages = self.get_pages()
+        initial = {p:True for p in pages}
+        context['form'] = SubscriptionForm(pages=pages, initial=initial)
         return context
+    
+    def post(self, request, *args, **kwargs):
+        pages = self.get_pages()
+        form = SubscriptionForm(pages=pages, data=request.POST)
+        if form.is_valid():
+            form.save(student=request.user.student)
+            messages.success(request, "Vos choix ont bien été enregistrés !")
+            new_pages = self.get_pages()
+            form = SubscriptionForm(new_pages, request.POST)
+        else:
+            messages.error(request, "Oups, une erreur c'est produite !")
+        context={'form':form}
+        return self.render_to_response(context)
