f7c31b0d83f144fdc54c02d28fdc86c4f2079622
Alexis Delage <alexidelage@gmail.com>
2022-06-18 22:08:05 +0200

    Correct indentation

--- frontend/src/components/notFound.tsx
+++ frontend/src/components/notFound.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function NotFound () {
-    return <h1>Page 404 NOT FOUND</h1>
+function NotFound() {
+  return <h1>Page 404 NOT FOUND</h1>;
 }
 
 export default NotFound;
--- frontend/src/languages/config.ts
+++ frontend/src/languages/config.ts
@@ -1,9 +1,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import translationFr from './fr-FR.json';
 import translationEn from './en-GB.json';
-
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
   // detect user language
@@ -15,7 +14,7 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     supportedLngs: ['fr-FR', 'en-GB'],
-    fallbackLng: {'fr-*': ['fr-FR'], 'default': ['en-GB']},
+    fallbackLng: { 'fr-*': ['fr-FR'], default: ['en-GB'] },
     debug: true,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
@@ -26,9 +25,8 @@ i18n
       },
       'en-GB': {
         translation: translationEn,
-      }
-    }
+      },
+    },
   });
-
 
 export default i18n;
