d987d5e3a83c1e671fa5deefe03a27be16d5bf10
charles-zablit <zablitcharles@gmail.com>
2022-07-28 14:20:55 +0200

    Update: vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  root: "public",
+  plugins: [react()]
 });
