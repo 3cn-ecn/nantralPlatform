e4e4b6957d3f48b9f587aeef51223551aa310a47
Alexis Delage <alexidelage@gmail.com>
2022-08-27 12:10:43 +0200

    correct slug of pictures

--- backend/apps/utils/upload.py
+++ backend/apps/utils/upload.py
@@ -1,4 +1,5 @@
 import os
+from typing import Any
 from uuid import uuid4
 from django.utils.deconstruct import deconstructible
 
@@ -10,6 +11,7 @@ class PathAndRename(object):
         self.path = sub_path
 
     def __call__(self, instance, filename):
+        print(filename)
         ext = filename.split('.')[-1]
         from apps.group.models import Group
         if isinstance(instance, Group):
@@ -17,13 +19,35 @@ class PathAndRename(object):
         else:
             dir = self.path
         # get filename
-        try:
-            filename = f'{instance.full_slug}.{ext}'
-        except AttributeError:
-            try:
-                filename = f'{instance.pk}.{ext}'
-            except AttributeError:
-                # set filename as random string
-                filename = f'{uuid4().hex}.{ext}'
+        new_name = ''
+        if try_to_get(instance, 'full_slug'):
+            new_name = f'{instance.full_slug}.{ext}'
+        elif try_to_get(instance, 'slug'):
+            new_name = f'{instance.slug}.{ext}'
+        elif try_to_get(instance, 'pk'):
+            new_name = f'{instance.pk}.{ext}'
+        else:
+            new_name = f'{uuid4().hex}.{ext}'
         # return the whole path to the file
-        return os.path.join(dir, filename)
+        return os.path.join(dir, new_name)
+
+
+def try_to_get(object: Any, attribute: str):
+    """Try to get an attribute and return null if the attribute does not exists. 
+
+    Parameters
+    ----------
+    object : Any
+        An object
+    attribute : str
+        The name of the attribute
+
+    Returns
+    -------
+    value
+        The value of attribute
+    """
+    try:
+        return getattr(object, attribute)
+    except Exception:
+        return None
