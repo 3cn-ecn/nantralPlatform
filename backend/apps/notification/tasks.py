bd312f5cf029157d1ddbf3d707ab96e29740c4c4
Alexis Delage <alexidelage@gmail.com>
2022-09-06 19:02:57 +0200

    linting

--- backend/apps/notification/tasks.py
+++ backend/apps/notification/tasks.py
@@ -12,10 +12,10 @@ from push_notifications.webpush import WebPushError
 @shared_task
 def send_webpush_notification_task(student_ids: List[int], message: dict):
     """The celery task for sending notifications to studends."""
-    
+
     # get devices from students
     devices = WebPushDevice.objects.filter(
-        user__student__id__in = student_ids
+        user__student__id__in=student_ids
     )
     # convert the message in json
     data = json.dumps(message)
@@ -25,7 +25,7 @@ def send_webpush_notification_task(student_ids: List[int], message: dict):
             device.send_message(message=data)
         except WebPushError as e:
             # retrive the server response
-            result = re.search('Push failed: ([\d]+) ', e.args[0])
+            result = re.search(r'Push failed: ([\d]+) ', e.args[0])
             error = int(result.group(1))
             # if device is no longer subscribed, delete it
             if error in [404, 410]:
@@ -37,11 +37,10 @@ def clean_notifications():
     """A simple celery task to delete the old notifications"""
 
     # we decide to delete all notifications older than 60 days
-    timeperiod = datetime.timedelta(day = 60)
+    timeperiod = datetime.timedelta(day=60)
     # then get the day of today
     today = timezone.now()
     # import the Notification model
     from .models import Notification
     # and finally delete the old notifications
-    Notification.objects.filter(date__lt = today - timeperiod).delete()
-
+    Notification.objects.filter(date__lt=today - timeperiod).delete()
