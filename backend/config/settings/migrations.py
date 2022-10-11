94ea9e63eed332ba93ea394628c752e718b90b20
Alexis Delage <alexidelage@gmail.com>
2022-10-11 21:47:18 +0200

    update config flake8

--- backend/config/settings/migrations.py
+++ backend/config/settings/migrations.py
@@ -1,6 +1,6 @@
 """A special config for makemigrations to not migrate features
 that were meant only for debug."""
 
-from .dev_local import *
+from .dev_local import *  # noqa: F401
 
 DEBUG = False
