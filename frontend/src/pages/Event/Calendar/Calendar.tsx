27f95361f9b1ca7236c1466530477ec9405db35a
ErwanQui <erwan.quinot@gmail.com>
2023-01-27 11:24:48 +0100

    Update Calendar display

--- frontend/src/pages/Event/Calendar/Calendar.tsx
+++ frontend/src/pages/Event/Calendar/Calendar.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { Box, Tab } from '@mui/material';
 import { TabContext, TabList, TabPanel } from '@mui/lab';
-// import './Event.scss';
+import './Calendar.scss';
 
 function TimeBlock(props: { startTime: any }) {
   const { startTime } = props;
+  if (startTime % 2 === 0) {
+    return (
+      <Box
+        className="TimeBlockEven"
+        // style={{ width: '120px', height: '40px', border: '1px solid gray' }}
+      >
+        {/* {startTime} */}
+        {/* <div id="trait_dessus"></div> */}
+      </Box>
+    );
+  }
+
   return (
-    <Box style={{ width: '120px', height: '40px', border: '1px solid gray' }}>
-      {startTime}
+    <Box
+      className="TimeBlockOdd"
+      // style={{ width: '120px', height: '40px', border: '1px solid gray' }}
+    >
+      {/* {startTime} */}
       {/* <div id="trait_dessus"></div> */}
     </Box>
   );
@@ -32,23 +47,58 @@ function DayInfos() {
   );
 }
 
-function Day(props: { day: string }) {
-  const { day } = props;
+function sortInDay(oldSortEvents: Array<any>) {
+  // const sortEvents = new Array(7).fill(Array<any>());
+  const sortEvents = new Array<any>();
+  for (let i = 0; i < 24; i++) {
+    sortEvents.push(new Array<any>());
+  }
+  let eventDate;
+  console.log(sortEvents);
+  oldSortEvents.forEach((event) => {
+    eventDate = new Date(event.date);
+    console.log(eventDate.getHour());
+    console.log(sortEvents[0]);
+    sortEvents[eventDate.getHour()].push(event);
+    console.log(sortEvents);
+  });
+  return sortEvents;
+}
+
+function Day(props: { day: string; events: any }) {
+  const { day, events } = props;
+  let dayChain;
+  for (let hour = 0; hour < 24; hour++) {
+    dayChain += <TimeBlock startTime={0}></TimeBlock>;
+  }
   return (
     <div style={{ display: 'block' }}>
       {day}
-      <TimeBlock startTime={0}></TimeBlock>
+      {dayChain}
+      {/* <TimeBlock startTime={0}></TimeBlock>
+      <TimeBlock startTime={1}></TimeBlock>
       <TimeBlock startTime={2}></TimeBlock>
+      <TimeBlock startTime={3}></TimeBlock>
       <TimeBlock startTime={4}></TimeBlock>
+      <TimeBlock startTime={5}></TimeBlock>
       <TimeBlock startTime={6}></TimeBlock>
+      <TimeBlock startTime={7}></TimeBlock>
       <TimeBlock startTime={8}></TimeBlock>
+      <TimeBlock startTime={9}></TimeBlock>
       <TimeBlock startTime={10}></TimeBlock>
+      <TimeBlock startTime={11}></TimeBlock>
       <TimeBlock startTime={12}></TimeBlock>
+      <TimeBlock startTime={13}></TimeBlock>
       <TimeBlock startTime={14}></TimeBlock>
+      <TimeBlock startTime={15}></TimeBlock>
       <TimeBlock startTime={16}></TimeBlock>
+      <TimeBlock startTime={17}></TimeBlock>
       <TimeBlock startTime={18}></TimeBlock>
+      <TimeBlock startTime={19}></TimeBlock>
       <TimeBlock startTime={20}></TimeBlock>
+      <TimeBlock startTime={21}></TimeBlock>
       <TimeBlock startTime={22}></TimeBlock>
+      <TimeBlock startTime={23}></TimeBlock> */}
     </div>
   );
 }
@@ -86,14 +136,18 @@ function getEventWithDate(events: Array<any>, beginDate: Date, endDate: Date) {
 }
 
 function sortInWeek(oldSortEvents: Array<any>) {
-  const sortEvents = new Array(7).fill(Array<any>());
+  // const sortEvents = new Array(7).fill(Array<any>());
+  const sortEvents = new Array<any>();
+  for (let i = 0; i < 7; i++) {
+    sortEvents.push(new Array<any>());
+  }
   let eventDate;
   console.log(sortEvents);
   oldSortEvents.forEach((event) => {
     eventDate = new Date(event.date);
     console.log(eventDate.getDay());
     console.log(sortEvents[0]);
-    sortEvents[eventDate.getDay()].push(event);
+    sortEvents[eventDate.getDay() - 1].push(event);
     console.log(sortEvents);
   });
   return sortEvents;
@@ -113,15 +167,15 @@ function Calendar(props: { events: any }) {
     <>
       {/* <h1>Évènements</h1> */}
       <p>Le calendrier</p>
-      <div style={{ display: 'flex' }}>
+      <div id="Calendar" style={{ display: 'flex' }}>
         <DayInfos />
-        <Day day="Lundi" />
-        <Day day="Mardi" />
-        <Day day="Mercredi" />
-        <Day day="Jeudi" />
-        <Day day="Vendredi" />
-        <Day day="Samedi" />
-        <Day day="Dimanche" />
+        <Day day="Lundi" events={sortEvents[0]} />
+        <Day day="Mardi" events={sortEvents[1]} />
+        <Day day="Mercredi" events={sortEvents[2]} />
+        <Day day="Jeudi" events={sortEvents[3]} />
+        <Day day="Vendredi" events={sortEvents[4]} />
+        <Day day="Samedi" events={sortEvents[5]} />
+        <Day day="Dimanche" events={sortEvents[6]} />
       </div>
     </>
   );
