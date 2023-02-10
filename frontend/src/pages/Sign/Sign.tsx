1fb3bd2cd745546862ce52f3aa074a2cb1201e46
EugeneCastelneau <95407459+EugeneCastelneau@users.noreply.github.com>
2023-02-10 09:40:49 +0100

    no imports

--- frontend/src/pages/Sign/Sign.tsx
+++ frontend/src/pages/Sign/Sign.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon';
 import Container from '@mui/material/Container';
-import { ReactComponent as NantralIcon } from '../../assets/logo/scalable/logo.svg';
 
 /**
  * Home Page, with Welcome message, next events, etc...
