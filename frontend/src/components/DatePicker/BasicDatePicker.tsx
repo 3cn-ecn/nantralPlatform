dc5ac2e67534b532744fcdb58ea3034e9bbf2981
ManonC12 <manon-c.coutier@wanadoo.fr>
2023-02-22 16:38:43 +0100

    Translation + DatePicker improvment
    - Translation of the filterbar
    - Smaller DatePicker, with a clear button
    - Removal of the titles of the datePicker and addition of labels instead
    - DesktopDatePicker for every media

--- frontend/src/components/DatePicker/BasicDatePicker.tsx
+++ frontend/src/components/DatePicker/BasicDatePicker.tsx
@@ -1,19 +1,24 @@
 import * as React from 'react';
+import { useTranslation } from 'react-i18next';
 import { Dayjs } from 'dayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import 'dayjs/locale/fr';
-import TextField from '@mui/material/TextField';
+import TextField, { TextFieldProps } from '@mui/material/TextField';
+import './BasicDatePicker.scss';
+import { IconButton, InputAdornment } from '@mui/material';
+import CancelIcon from '@mui/icons-material/Cancel';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 // TO DO : UN BOUTON POUR ENLEVER LA DATE CHOISIE
-// TO DO : make adapterLocale and toolbarTitle adapt to the language selected
 export default function BasicDatePicker(props: {
   label: string;
   minDate: any;
   getDate: any;
 }) {
-  const [valueDebut, setValue] = React.useState<Dayjs | null>(null);
+  const { t } = useTranslation('translation'); // translation module
+  const [value, setValue] = React.useState<Dayjs | null>(null);
   const { label, minDate, getDate } = props;
 
   const handleChange = (newValue: Dayjs | null) => {
@@ -21,19 +26,46 @@ export default function BasicDatePicker(props: {
     getDate(newValue);
   };
 
+  const clearDate = () => {
+    setValue(null);
+    getDate(null);
+  };
+
   return (
     <LocalizationProvider
       dateAdapter={AdapterDayjs}
-      adapterLocale="fr"
-      style={{ width: 230 }}
+      adapterLocale={t('date_picker.language')}
     >
-      <DatePicker
+      <DesktopDatePicker
         minDate={minDate}
         label={label}
-        value={valueDebut}
+        value={value}
         onChange={handleChange}
-        toolbarTitle="dd/mm/yyyy"
-        renderInput={(params) => <TextField {...params} />}
+        showDaysOutsideCurrentMonth
+        renderInput={(params) => (
+          <TextField
+            className="textfield"
+            size="small"
+            {...params}
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <>
+                  <InputAdornment className="adornment" position="end">
+                    <IconButton
+                      className="button"
+                      size="small"
+                      onClick={clearDate}
+                    >
+                      <CancelIcon fontSize="small" />
+                    </IconButton>
+                  </InputAdornment>
+                  {params.InputProps.endAdornment}
+                </>
+              ),
+            }}
+          ></TextField>
+        )}
       />
     </LocalizationProvider>
   );
