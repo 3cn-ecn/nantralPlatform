ee250c00d60dc7abd4b5773aa5f02b75951309cd
remi ravelli <remi.ravelli@gmail.com>
2023-02-24 11:04:44 +0100

    Removed code duplication

--- frontend/src/components/ClubAvatar/ClubAvatar.tsx
+++ frontend/src/components/ClubAvatar/ClubAvatar.tsx
@@ -1,7 +1,7 @@
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton/IconButton';
 import * as React from 'react';
-import { stringToColor } from '../Avatar/Avatar';
+import { stringToColor } from '../../utils/formatText';
 
 function ClubAvatar(props: {
   name: string;
