ada33e39ba7d8df53e72134b776919210cb6e041
ErwanQui <erwan.quinot@gmail.com>
2023-02-27 11:31:41 +0100

    update jest to get coverage ?

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -7,4 +7,6 @@ export default {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
   },
   testEnvironment: 'jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.(tsx|ts)'],
 };
