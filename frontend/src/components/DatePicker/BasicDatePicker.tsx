16b4b06a8746df316d54cb093e87794215ecdc6c
ManonC12 <112811926+ManonC12@users.noreply.github.com>
2023-02-27 10:44:15 +0100

    React app filter translation datepicker (#943)
    * Translation + DatePicker improvment
    
    - Translation of the filterbar
    - Smaller DatePicker, with a clear button
    - Removal of the titles of the datePicker and addition of labels instead
    - DesktopDatePicker for every media
    
    * DatePicker clear button improvement
    
    - the clear button of the DatePicker is displayed only when the picker has a value (is not empty)
    
    * Fix errors
    
    - added a translation

--- frontend/src/components/DatePicker/BasicDatePicker.tsx
+++ frontend/src/components/DatePicker/BasicDatePicker.tsx
@@ -1,39 +1,91 @@
 import * as React from 'react';
+import { useTranslation } from 'react-i18next';
 import { Dayjs } from 'dayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import 'dayjs/locale/fr';
 import TextField from '@mui/material/TextField';
+import './BasicDatePicker.scss';
+import { IconButton, InputAdornment } from '@mui/material';
+import CancelIcon from '@mui/icons-material/Cancel';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 
-// TO DO : UN BOUTON POUR ENLEVER LA DATE CHOISIE
-// TO DO : make adapterLocale and toolbarTitle adapt to the language selected
 export default function BasicDatePicker(props: {
   label: string;
   minDate: any;
   getDate: any;
 }) {
-  const [valueDebut, setValue] = React.useState<Dayjs | null>(null);
+  const { t } = useTranslation('translation'); // translation module
+  const [value, setValue] = React.useState<Dayjs | null>(null);
+  const [isEmpty, setIsEmpty] = React.useState(true);
   const { label, minDate, getDate } = props;
 
   const handleChange = (newValue: Dayjs | null) => {
     setValue(newValue);
-    getDate(newValue);
+    if (newValue === null) {
+      setIsEmpty(true);
+    } else {
+      setIsEmpty(false);
+    }
+  };
+
+  const clearDate = () => {
+    setValue(null);
+    getDate(null);
+    setIsEmpty(true);
+  };
+
+  /**
+   * Function used to render DesktopDatePicker
+   * @param params params of DesktopDatePicker
+   * @param empty boolean : true if picker is empty
+   * @returns the input rendered
+   */
+  const render = (params: any, empty: boolean) => {
+    // if picker is not empty, display a clear button
+    if (empty === false) {
+      return (
+        <TextField
+          className="textfield"
+          size="small"
+          {...params}
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <>
+                <InputAdornment className="adornment" position="end">
+                  <IconButton
+                    className="button"
+                    size="small"
+                    onClick={clearDate}
+                  >
+                    <CancelIcon fontSize="small" />
+                  </IconButton>
+                </InputAdornment>
+                {params.InputProps.endAdornment}
+              </>
+            ),
+          }}
+        ></TextField>
+      );
+    }
+    return (
+      <TextField className="textfield" size="small" {...params}></TextField>
+    );
   };
 
   return (
     <LocalizationProvider
       dateAdapter={AdapterDayjs}
-      adapterLocale="fr"
-      style={{ width: 230 }}
+      adapterLocale={t('date_picker.language')}
     >
-      <DatePicker
+      <DesktopDatePicker
         minDate={minDate}
         label={label}
-        value={valueDebut}
+        value={value}
         onChange={handleChange}
-        toolbarTitle="dd/mm/yyyy"
-        renderInput={(params) => <TextField {...params} />}
+        showDaysOutsideCurrentMonth
+        renderInput={(params) => render(params, isEmpty)}
       />
     </LocalizationProvider>
   );
