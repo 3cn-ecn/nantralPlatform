1b5478f86b6bfa841e186848bb92b85a6403c91e
ErwanQui <erwan.quinot@gmail.com>
2023-03-04 16:36:49 +0100

    Can't delete because some components used it

--- frontend/src/components/Calendar/utils.tsx
+++ frontend/src/components/Calendar/utils.tsx
@@ -1,12 +1,3 @@
-export function isThisWeek(date: Date): boolean {
-  const todayTime: number = date.getTime();
-  const monday = new Date();
-  monday.setDate(monday.getDate() - monday.getDay() + 1);
-  const sunday = new Date();
-  sunday.setDate(sunday.getDate() - sunday.getDay() + 8);
-  return todayTime >= monday.getTime() && todayTime <= sunday.getTime();
-}
-
 /**
  * Get the number of days in a month.
  * @param year The year of the month.
