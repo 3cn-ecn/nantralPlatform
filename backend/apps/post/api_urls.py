24fafb14c0a61a0f940dbf92358e86772fd7ce3d
remi ravelli <remi.ravelli@gmail.com>
2023-03-07 00:01:01 +0100

    removed code smells

--- backend/apps/post/api_urls.py
+++ backend/apps/post/api_urls.py
@@ -1,4 +1,3 @@
-# from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from .api_views import (
@@ -8,12 +7,5 @@ from .api_views import (
 app_name = "post"
 router = DefaultRouter()
 router.register('', PostViewSet, basename='event')
-"""
-urlpatterns = [
-    path('', ListPostsAPIView.as_view({'get': 'list'}), name='list-all-posts'),
-    path('<slug:post_slug>', UpdatePostAPIView.as_view(), name='update-post'),
-    path('group/<slug:group>', ListPostsGroupAPIView.as_view(),
-         name='list-group-posts'),
-]
-"""
+
 urlpatterns = router.urls
