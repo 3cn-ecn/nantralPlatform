09ccfa9eb0c4404217b13b6cfbbc57f29faa50ca
remi ravelli <remi.ravelli@gmail.com>
2023-03-06 23:36:27 +0100

    backend for post

--- backend/apps/post/serializers.py
+++ backend/apps/post/serializers.py
@@ -6,18 +6,27 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     group_slug = serializers.SerializerMethodField()
 
+    def validate(self, attrs):
+        if (not attrs["group"].is_admin(self.context['request'].user)):
+            raise serializers.ValidationError(
+                "You have to be admin to add or update an event")
+        return super().validate(attrs)
+
     class Meta:
         model = Post
+        read_only_fields = ['slug']
         fields = [
+            'id',
+            'slug',
             'title',
             'publication_date',
+            'group',
+            'group_slug',
             'color',
             'image',
-            'slug',
             'publicity',
             'pinned',
             'page_suggestion',
-            'group_slug',
             'description',
         ]
 
