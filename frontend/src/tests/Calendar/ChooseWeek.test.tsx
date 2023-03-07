2a9af08f39a0444b37914e85b35ec274cee11097
ErwanQui <erwan.quinot@gmail.com>
2023-03-07 13:58:55 +0100

    implement ChooseWeek snapshot

--- frontend/src/tests/Calendar/ChooseWeek.test.tsx
+++ frontend/src/tests/Calendar/ChooseWeek.test.tsx
@@ -1,18 +1,43 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { EventProps } from '../../Props/Event';
-import { createTestEvent } from './testElements/testElements';
+import { CalendarView } from 'components/Calendar/CalendarProps/CalendarProps';
 import { ChooseWeek } from '../../components/Calendar/ChooseWeek/ChooseWeek';
 
-const event: EventProps = createTestEvent();
-// const day: { day: number; date: number; events: Array<EventProps> }
+let beginDate = new Date('2023-02-17T03:24:00');
+const setBeginDate = (newDate: Date) => {
+  beginDate = newDate;
+};
+let endDate = new Date('2023-02-22T03:24:00');
+const setEndDate = (newDate: Date) => {
+  endDate = newDate;
+};
+let displayData: {
+  type: CalendarView;
+  beginDate: number;
+} = {
+  type: 'week',
+  beginDate: 0,
+};
+const updateDisplay = (newDisplay: {
+  type: CalendarView;
+  beginDate: number;
+}) => {
+  displayData = newDisplay;
+};
+
 describe('<ChooseWeek />', () => {
   it('should display an ChooseWeek', async () => {
-    // const component = render(
-    //   <ChooseWeek key="ChooseWeekTest" day={null} maxEventsInDayWeek={3} inMonth />
-    // );
-    // expect(component.getByRole('button').style.height).toBe('20px');
-    // expect(component.getByRole('button').style.padding).toBe('0px');
-    // expect(component).toMatchSnapshot();
+    const component = render(
+      <ChooseWeek
+        key="ChooseWeekTest"
+        step={displayData}
+        updateDisplay={updateDisplay}
+        beginDate={beginDate}
+        endDate={endDate}
+        updateBegin={setBeginDate}
+        updateEnd={setEndDate}
+      />
+    );
+    expect(component).toMatchSnapshot();
   });
 });
