48373c2bcfd12d404af5d62b6ca7448cdf15407f
ErwanQui <erwan.quinot@gmail.com>
2023-03-07 14:41:10 +0100

    Improve EventBlock tests

--- frontend/src/tests/Calendar/EventBlock.test.tsx
+++ frontend/src/tests/Calendar/EventBlock.test.tsx
@@ -4,13 +4,70 @@ import { EventProps } from '../../Props/Event';
 import { createTestEvent } from './testElements/testElements';
 import { EventBlock } from '../../components/Calendar/Day/EventBlock/EventBlock';
 
-const event: EventProps = createTestEvent();
+const eventInDay: EventProps = createTestEvent(
+  'The slug',
+  'The test event',
+  new Date('2023-02-17T03:24:00'),
+  new Date('2023-02-17T04:24:00')
+);
+const eventBeginInDay: EventProps = createTestEvent(
+  'The slug',
+  'The test event',
+  new Date('2023-02-17T03:00:00'),
+  new Date('2023-03-18T04:24:00')
+);
+const eventEndInDay: EventProps = createTestEvent(
+  'The slug',
+  'The test event',
+  new Date('2023-02-16T03:24:00'),
+  new Date('2023-02-17T04:00:00')
+);
+const eventMiddleInDay: EventProps = createTestEvent(
+  'The slug',
+  'The test event',
+  new Date('2023-02-16T03:24:00'),
+  new Date('2023-02-18T04:24:00')
+);
+
 describe('<EventBlock />', () => {
-  it('should display an EventBlock', async () => {
+  it('should display an EventBlock in the day', async () => {
     const component = render(
-      <EventBlock day={event.beginDate.getDay()} event={event} />
+      <EventBlock day={eventInDay.beginDate.getDay()} event={eventInDay} />
     );
     expect(component.getByRole('button').style.height).toBe('20px');
+    expect(component.getByRole('button').style.padding).toBe('0px');
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should display an EventBlock in the end of the day', async () => {
+    const component = render(
+      <EventBlock
+        day={eventBeginInDay.beginDate.getDay()}
+        event={eventBeginInDay}
+      />
+    );
+    expect(component.getByRole('button').style.height).toBe('420px');
+    expect(component.getByRole('button').style.padding).toBe('0px');
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should display an EventBlock in the begin the day', async () => {
+    const component = render(
+      <EventBlock day={eventEndInDay.endDate.getDay()} event={eventEndInDay} />
+    );
+    expect(component.getByRole('button').style.height).toBe('80px');
+    expect(component.getByRole('button').style.padding).toBe('0px');
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should display an EventBlock in the all day', async () => {
+    const component = render(
+      <EventBlock
+        day={eventMiddleInDay.beginDate.getDay() + 1}
+        event={eventMiddleInDay}
+      />
+    );
+    expect(component.getByRole('button').style.height).toBe('480px');
     expect(component.getByRole('button').style.padding).toBe('0px');
     expect(component).toMatchSnapshot();
   });
