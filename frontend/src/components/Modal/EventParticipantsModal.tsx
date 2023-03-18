3473d0dfb95b4b1b58065b5be0ab4eef83ced7a5
Inky19 <f.marie260@gmail.com>
2023-03-18 21:20:27 +0100

    Fixed code smell

--- frontend/src/components/Modal/EventParticipantsModal.tsx
+++ frontend/src/components/Modal/EventParticipantsModal.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
 import { Container } from '@mui/system';
