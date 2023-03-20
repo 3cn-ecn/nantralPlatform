a2340d3a63736848512d1ce6f65010fda616ecf2
ErwanQui <erwan.quinot@gmail.com>
2023-03-20 14:42:17 +0100

    update tests

--- frontend/src/components/Calendar/DayInfos/DayInfos.tsx
+++ frontend/src/components/Calendar/DayInfos/DayInfos.tsx
@@ -7,15 +7,27 @@ import './DayInfos.scss';
  */
 export function DayInfos() {
   const timeSlots = [];
-  for (let i = 0; i <= 10; i += 2) {
-    timeSlots.push(<p className="timeSlot">{`0${i}:00`}</p>);
+  for (let i = 0; i < 10; i += 2) {
+    timeSlots.push(
+      <p
+        key={`timeSlot 0${i}:00`}
+        className="timeSlot"
+        data-testid={`timeSlotTestId 0${i}:00`}
+      >{`0${i}:00`}</p>
+    );
   }
   for (let i = 10; i < 24; i += 2) {
-    timeSlots.push(<p className="timeSlot">{`${i}:00`}</p>);
+    timeSlots.push(
+      <p
+        key={`timeSlot ${i}:00`}
+        className="timeSlot"
+        data-testid={`timeSlotTestId ${i}:00`}
+      >{`${i}:00`}</p>
+    );
   }
   return (
-    <div className="blockDisplay">
-      <div className="blankedArea"></div>
+    <div className="blockDisplay" data-testid="blockDisplayTestId">
+      <div className="blankedArea" data-testid="blankedAreaTestId"></div>
       {timeSlots}
     </div>
   );
