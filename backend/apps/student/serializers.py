cecf5ecc4b719de3abad9ad75715d2cef5eb6323
Corentin <coco@clementmacfarlane.fr>
2023-03-20 11:44:21 +0100

    modif staff

--- backend/apps/student/serializers.py
+++ backend/apps/student/serializers.py
@@ -6,16 +6,21 @@ from .models import Student
 class StudentSerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField()
     url = serializers.SerializerMethodField()
+    staff = serializers.SerializerMethodField()
 
     class Meta:
         model = Student
-        fields = ['id', 'name', 'promo', 'picture', 'faculty', 'path', 'url']
+        fields = ['id', 'name', 'promo', 'picture',
+                  'faculty', 'path', 'url', 'staff']
 
     def get_name(self, obj: Student) -> str:
         return obj.name
 
     def get_url(self, obj: Student) -> str:
         return obj.get_absolute_url()
+
+    def get_staff(self, obj: Student) -> bool:
+        return obj.staff
 
 
 class SimpleStudentSerializer(serializers.ModelSerializer):
