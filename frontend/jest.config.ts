83b7e12688f239a804ee792f6071acd74a29dccd
Alexis Delage <alexidelage+github@gmail.com>
2023-04-27 01:21:57 +0200

    correct tests

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -5,6 +5,11 @@ export default {
   moduleNameMapper: {
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '^#api/(.*)': '<rootDir>/src/api/$1',
+    '^#components/(.*)': '<rootDir>/src/components/$1',
+    '^#pages/(.*)': '<rootDir>/src/pages/$1',
+    '^#types/(.*)': '<rootDir>/src/types/$1',
+    '^#utils/(.*)': '<rootDir>/src/utils/$1',
   },
   testEnvironment: 'jsdom',
   maxWorkers: 2,
