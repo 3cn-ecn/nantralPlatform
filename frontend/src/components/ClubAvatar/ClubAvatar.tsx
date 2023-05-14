97e98f6c721e001d3fa9af9fcecb85ac47e4ae37
Alexis Delage <alexis.delage@eleves.ec-nantes.fr>
2023-05-14 23:06:41 +0200

    refacto: rename file

--- frontend/src/components/ClubAvatar/ClubAvatar.tsx
+++ frontend/src/components/ClubAvatar/ClubAvatar.tsx
@@ -4,7 +4,7 @@ import { Skeleton } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton/IconButton';
 
-import { stringToColor } from '#utils/formatText';
+import { stringToColor } from '#utils/stringToColor';
 
 export function ClubAvatar(props: {
   name: string;
