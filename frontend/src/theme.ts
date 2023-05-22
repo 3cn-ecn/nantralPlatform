e318ce5c8d42004c326356d4b7c3797e8a25df1d
Alexis Delage <alexidelage@gmail.com>
2023-05-22 23:36:28 +0200

    fix: correct theme

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -54,20 +54,9 @@ function getTheme(mode: PaletteMode) {
       },
     },
     shape: {
-      borderRadius: 15,
+      borderRadius: 20,
     },
     overrides: {
-      MuiButton: {
-        root: {
-          background: 'linear-gradient(45deg, #FE6B8B 30%, #0000FF 90%)',
-          border: 0,
-          borderRadius: 3,
-          boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-          color: 'white',
-          height: 48,
-          padding: '0 30px',
-        },
-      },
       MuiSwitch: {
         root: {
           width: 42,
