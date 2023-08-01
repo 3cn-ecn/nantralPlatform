211803ff8c3f600b2e209bcbd1b24d74097e3203
Kérian Fiter <kerian.fiter@gmail.com>
2023-08-01 12:49:50 +0200

    removed non_subscribed_members check

--- backend/apps/family/views.py
+++ backend/apps/family/views.py
@@ -254,12 +254,13 @@ class UpdateFamilyView(UserIsAdmin, TemplateView):
         ]
         if forms[0].is_valid() and forms[1].is_valid() and forms[2].is_valid():
             # on vérifie le nb de membres
-            non_subscribed_list = forms[0].cleaned_data[
-                'non_subscribed_members']
-            if non_subscribed_list:
-                nb_non_subscribed = len((non_subscribed_list).split(','))
-            else:
-                nb_non_subscribed = 0
+            # non_subscribed_list = forms[0].cleaned_data[
+            #     'non_subscribed_members']
+            # if non_subscribed_list:
+            #     nb_non_subscribed = len((non_subscribed_list).split(','))
+            # else:
+            #     nb_non_subscribed = 0
+            nb_non_subscribed = 0
             nb_subscribed = 0
             for form in forms[1]:
                 if hasattr(form.instance, 'student'):
