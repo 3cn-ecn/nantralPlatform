7c655c6ace40799af469a107eedb9fe6790a16c7
Alexis Delage <alexis.delage@eleves.ec-nantes.fr>
2023-09-13 00:35:39 +0200

    lint: apply black

--- backend/apps/account/manager.py
+++ backend/apps/account/manager.py
@@ -3,11 +3,11 @@ from django.utils import timezone
 
 
 class UserManager(BaseUserManager):
-
-    def _create_user(self, email, password, is_staff, is_superuser,
-                     **extra_fields):
+    def _create_user(
+        self, email, password, is_staff, is_superuser, **extra_fields
+    ):
         if not email:
-            raise ValueError('Users must have an email address')
+            raise ValueError("Users must have an email address")
         now = timezone.now()
         email = self.normalize_email(email)
         user = self.model(
@@ -24,8 +24,12 @@ class UserManager(BaseUserManager):
         return user
 
     def create_user(self, email, password, **extra_fields):
-        return self._create_user(email, password, False, False, **extra_fields)
+        return self._create_user(
+            email, password, is_staff=False, is_superuser=False, **extra_fields
+        )
 
     def create_superuser(self, email, password, **extra_fields):
-        user = self._create_user(email, password, True, True, **extra_fields)
+        user = self._create_user(
+            email, password, is_staff=True, is_superuser=True, **extra_fields
+        )
         return user
