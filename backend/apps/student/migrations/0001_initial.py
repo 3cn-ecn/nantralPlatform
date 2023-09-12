09e8c09ce1048459d30bef697fbb1256569e2680
Alexis Delage <alexis.delage@eleves.ec-nantes.fr>
2023-09-13 00:39:01 +0200

    fix: add User dependecy to student first migration

--- backend/apps/student/migrations/0001_initial.py
+++ backend/apps/student/migrations/0001_initial.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
     initial = True
 
-    dependencies = []
+    dependencies = [("account", "0001_create_test_account_dev")]
 
     operations = [
         migrations.CreateModel(
