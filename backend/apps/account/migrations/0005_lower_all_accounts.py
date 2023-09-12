a85ccbb9a75ef27b680fad5599e85746e035ed34
Alexis Delage <alexis.delage@eleves.ec-nantes.fr>
2023-09-13 00:38:10 +0200

    fix: import User from correct app

--- backend/apps/account/migrations/0005_lower_all_accounts.py
+++ backend/apps/account/migrations/0005_lower_all_accounts.py
@@ -10,7 +10,7 @@ Correcting this issue.
 def forwards_func(apps, schema_editor):
     # We get the model from the versioned app registry;
     # if we directly import it, it'll be the wrong version
-    User = apps.get_registered_model('auth', 'User')
+    User = apps.get_registered_model('account', 'User')
     for user in User.objects.all():
         if user.email is not None:
             user.email = user.email.lower()
