8f4e8517f50437023cdd4d6ae37b2adebfe38546
rravelli <remi.ravelli@gmail.com>
2023-09-16 17:43:06 +0200

    update views

--- backend/apps/account/views.py
+++ backend/apps/account/views.py
@@ -1,16 +1,16 @@
 import uuid
-from datetime import datetime
 from typing import Any, Dict
 from urllib.parse import urlparse
 
 from django.conf import settings
 from django.contrib import messages
-from django.contrib.auth import get_user_model, login, logout
+from django.contrib.auth import authenticate, get_user_model, login, logout
 from django.contrib.auth.forms import SetPasswordForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import PasswordResetConfirmView
 from django.http.response import HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
 from django.urls import reverse, reverse_lazy
 from django.utils import timezone
 from django.utils.encoding import force_bytes, force_str
@@ -22,7 +22,6 @@ from django.views.generic import FormView, TemplateView
 from apps.student.models import Student
 from apps.utils.send_email import send_email
 
-from .emailAuthBackend import EmailBackend
 from .forms import (
     ForgottenPassForm,
     LoginForm,
@@ -59,16 +58,12 @@ class TemporaryRegistrationView(FormView):
     template_name = "account/temporary_registration.html"
 
     def get(self, request, invite_id: uuid.UUID, *args, **kwargs):
-        """Do not allow to use this view outside of allowed temporary accounts
-        windows.
-        """
-        good_id = IdRegistration.objects.filter(id=invite_id).exists()
-        if (
-            timezone.now().today() > settings.TEMPORARY_ACCOUNTS_DATE_LIMIT
-            or not good_id
-        ):
+        # Do not allow to use this view outside of allowed.
+        # temporary accounts windows.
+        invitation = IdRegistration.objects.filter(id=invite_id).first()
+        if invitation is None or timezone.now() > invitation.expires_at:
             messages.error(
-                request, "Invitation invalide : le lien d'invitation a expiré."
+                request, "Invitation invalide : le lien d'invitation a expiré."
             )
             return redirect("account:registration-choice")
         return super().get(request, *args, **kwargs)
@@ -99,17 +94,16 @@ class ConfirmUser(View):
         except (TypeError, ValueError, OverflowError, User.DoesNotExist):
             return render(self.request, "account/activation_invalid.html")
         # get the associated temporary object if it exists
-        try:
-            temp_access_req = TemporaryAccessRequest.objects.get(user=user.pk)
-        except TemporaryAccessRequest.DoesNotExist:
-            temp_access_req = None
+        invitation = user.invitation is not None
         # checking if the token is valid.
         if account_activation_token.check_token(user, token):
             # if valid set active true
             user.is_active = True
-            if temp_access_req and temp_access_req.final_email:
-                user.email = temp_access_req.final_email
-                temp_access_req.delete()
+            user.is_email_valid = True
+            if invitation and user.email_next:
+                user.email = user.email_next
+                user.email_next = None
+                user.invitation = None
                 messages.warning(
                     request,
                     f"Dorénavant vous devez utiliser {user.email} pour vous "
@@ -152,60 +146,39 @@ class AuthView(FormView):
     def form_valid(self, form):
         username = form.cleaned_data["email"]
         password = form.cleaned_data["password"]
-        user = EmailBackend.authenticate(username=username, password=password)
+        user = authenticate(self.request, email=username, password=password)
 
         url = self.request.GET.get("next", "/")
         parsed_uri = urlparse(url)
         if parsed_uri.scheme != "" or parsed_uri.netloc != "":
             url = "/"
 
-        # Wrong credentials
-        if user is None:
+        # Wrong credentials or expired invitation
+        if user is None or (
+            user.invitation is not None
+            and user.invitation.expires_at < timezone.now()
+        ):
             messages.error(
                 self.request, "Identifiant inconnu ou mot de passe invalide."
             )
             return redirect("account:login")
 
-        # Normal case
-        if user.is_active:
-            message = f"Bonjour {user.first_name.title()} !"
-            messages.success(self.request, message)
-            login(
-                self.request,
-                user,
-                backend=AUTH_BACKEND,
-            )
-            # We send back the user to where he wanted to go or to home page
-            return redirect(url)
+        # Not verified email
+        if not user.is_email_valid:
+            self.request.session["email"] = username
+            return redirect("account:confirm-email")
 
-        # During certain periods allow temporary accounts.
+        # Temporary account
         if (
-            settings.TEMPORARY_ACCOUNTS_DATE_LIMIT >= timezone.now().today()
-            and TemporaryAccessRequest.objects.filter(user=user).exists()
+            user.invitation is not None
+            and user.invitation.expires_at > timezone.now()
         ):
-            temp_access_req = TemporaryAccessRequest.objects.get(user=user)
-
-            # Mail not verified
-            if not temp_access_req.mail_valid:
-                self.request.session["email"] = username
-                return redirect("account:confirm-email")
-
-            # Temporary period passed
-            if temp_access_req.approved_until <= datetime.now().date():
-                message = (
-                    "Votre compte n'a pas encore été approuvé. "
-                    "On vous prévient par mail dès que c'est le "
-                    "cas."
-                )
-                messages.error(self.request, message)
-                return redirect("account:login")
-
             message = (
                 "Votre compte n'est pas encore définitif. "
                 'Veuillez le valider <a href="'
                 f'{reverse("account:upgrade-permanent")}">ici</a>. '
                 "Attention après le "
-                f"{temp_access_req.approved_until} vous ne "
+                f"{user.invitation.expires_at} vous ne "
                 "pourrez plus vous connecter si vous n'avez pas "
                 "renseigné votre adresse Centrale."
             )
@@ -218,9 +191,16 @@ class AuthView(FormView):
             # We send back the user to where he wanted to go or to home page
             return redirect(url)
 
-        # Account is not temporary and not active, so email is not verified
-        self.request.session["email"] = username
-        return redirect("account:confirm-email")
+        # Normal case
+        message = f"Bonjour {user.first_name.title()} !"
+        messages.success(self.request, message)
+        login(
+            self.request,
+            user,
+            backend=AUTH_BACKEND,
+        )
+        # We send back the user to where he wanted to go or to home page
+        return redirect(url)
 
 
 class LogoutView(View):
@@ -293,17 +273,16 @@ class PermanentAccountUpgradeView(LoginRequiredMixin, FormView):
     success_url = reverse_lazy("home:home")
 
     def get(self, request):
-        get_object_or_404(TemporaryAccessRequest, user=self.request.user)
-        return super().get(request)
+        if request.user.invitation is None:
+            redirect("/")
+        return super().get(request=request)
 
     def form_valid(self, form: UpgradePermanentAccountForm) -> HttpResponse:
-        temp_request = get_object_or_404(
-            TemporaryAccessRequest, user=self.request.user
-        )
-        temp_request.final_email = form.cleaned_data["email"]
-        temp_request.save()
+        new_email = form.cleaned_data["email"]
+        self.request.user.email_next = new_email
+        self.request.user.save()
         send_email_confirmation(
-            self.request.user, self.request, send_to=form.cleaned_data["email"]
+            self.request.user, self.request, send_to=new_email
         )
         return super().form_valid(form)
 
