f00ded4efb0a7463fa9f40cdba2a9eeece680cd9
Corentin <coco@clementmacfarlane.fr>
2023-09-25 16:43:36 -0400

    feat: Add Language Selector to components

---
+++ frontend/src/shared/components/LanguageSelector/LanguageSelector.tsx
@@ -0,0 +1,62 @@
+import { useState, MouseEvent, Dispatch } from 'react';
+
+import { KeyboardArrowDown as KeyboardArrowDownIcon } from '@mui/icons-material';
+import { Button, Menu, MenuItem } from '@mui/material';
+
+import { languages_without_locales } from '#shared/i18n/config';
+import { useTranslation } from '#shared/i18n/useTranslation';
+import { getLanguageName } from '#shared/utils/getLanguageName';
+
+type LanguageSelectorProps = {
+  selectedLang: string;
+  setSelectedLang: Dispatch<React.SetStateAction<string>>;
+};
+
+export function LanguageSelector({
+  selectedLang,
+  setSelectedLang,
+}: LanguageSelectorProps) {
+  const { i18n } = useTranslation();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const isOpen = Boolean(anchorEl);
+  const handleClick = (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <>
+      <Button
+        variant="outlined"
+        disableElevation
+        onClick={handleClick}
+        endIcon={<KeyboardArrowDownIcon />}
+      >
+        {selectedLang}
+      </Button>
+      <Menu
+        id="demo-customized-menu"
+        MenuListProps={{
+          'aria-labelledby': 'demo-customized-button',
+        }}
+        anchorEl={anchorEl}
+        open={isOpen}
+        onClose={handleClose}
+      >
+        {languages_without_locales.map((language) => (
+          <MenuItem
+            key={language}
+            value={language}
+            onClick={() => {
+              setSelectedLang(language);
+              handleClose();
+            }}
+          >
+            {getLanguageName(language, i18n.language)}
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
+  );
+}
