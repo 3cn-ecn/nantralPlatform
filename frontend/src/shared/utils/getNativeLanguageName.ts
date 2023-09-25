e4e640f0acde348fdfb567b6a571899ecd205218
Corentin <coco@clementmacfarlane.fr>
2023-09-25 16:43:19 -0400

    fix: Update getNativeLanguageName

--- frontend/src/shared/utils/getNativeLanguageName.ts
+++ frontend/src/shared/utils/getNativeLanguageName.ts
@@ -1,16 +1,5 @@
-import { upperFirst } from 'lodash-es';
+import { getLanguageName } from '#shared/utils/getLanguageName';
 
 export const getNativeLanguageName = (lang: string): string => {
-  try {
-    // try to get names for the asking language
-    const displayName = new Intl.DisplayNames(lang, {
-      type: 'language',
-      style: 'short',
-      languageDisplay: 'standard',
-    });
-    return upperFirst(displayName.of(lang)) || lang;
-  } catch {
-    // if lang argument is not a language code
-    return lang;
-  }
+  return getLanguageName(lang, lang);
 };
