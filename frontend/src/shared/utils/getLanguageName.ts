18414372eabfe172b3858d120c9fa06d1d07f19e
Corentin <coco@clementmacfarlane.fr>
2023-09-25 16:43:00 -0400

    feat: add getLanguageName

---
+++ frontend/src/shared/utils/getLanguageName.ts
@@ -0,0 +1,19 @@
+import { upperFirst } from 'lodash-es';
+
+export const getLanguageName = (
+  lang: string,
+  displayedLang: string
+): string => {
+  try {
+    // try to get names for the asking language
+    const displayName = new Intl.DisplayNames(displayedLang, {
+      type: 'language',
+      style: 'short',
+      languageDisplay: 'standard',
+    });
+    return upperFirst(displayName.of(lang)) || lang;
+  } catch {
+    // if lang argument is not a language code
+    return lang;
+  }
+};
