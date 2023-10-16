0f9646539e4c18053877999e29aab7d7d6afcfa9
Alexis Delage <alexis.delage@eleves.ec-nantes.fr>
2023-10-16 15:00:19 +0200

    refacto: clean code for language selector

--- frontend/src/shared/components/LanguageSelector/LanguageSelector.tsx
+++ frontend/src/shared/components/LanguageSelector/LanguageSelector.tsx
@@ -1,15 +1,18 @@
-import { useState, MouseEvent, Dispatch } from 'react';
+import { MouseEvent, useState } from 'react';
 
 import { KeyboardArrowDown as KeyboardArrowDownIcon } from '@mui/icons-material';
 import { Button, Menu, MenuItem } from '@mui/material';
+import { uniq } from 'lodash-es';
 
-import { base_languages } from '#shared/i18n/config';
+import { languages } from '#shared/i18n/config';
 import { useTranslation } from '#shared/i18n/useTranslation';
 import { getLanguageName } from '#shared/utils/getLanguageName';
 
+const base_languages = uniq(languages.map((lg) => lg.split('-')[0]));
+
 interface LanguageSelectorProps {
   selectedLang: string;
-  setSelectedLang: Dispatch<React.SetStateAction<string>>;
+  setSelectedLang: (lang: string) => void;
 }
 
 export function LanguageSelector({
@@ -17,33 +20,27 @@ export function LanguageSelector({
   setSelectedLang,
 }: LanguageSelectorProps) {
   const { i18n } = useTranslation();
+
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const isOpen = Boolean(anchorEl);
+
   const handleClick = (event: MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
     setAnchorEl(null);
   };
+
   return (
     <>
       <Button
         variant="outlined"
-        disableElevation
         onClick={handleClick}
         endIcon={<KeyboardArrowDownIcon />}
       >
         {selectedLang}
       </Button>
-      <Menu
-        id="demo-customized-menu"
-        MenuListProps={{
-          'aria-labelledby': 'demo-customized-button',
-        }}
-        anchorEl={anchorEl}
-        open={isOpen}
-        onClose={handleClose}
-      >
+      <Menu anchorEl={anchorEl} open={isOpen} onClose={handleClose}>
         {base_languages.map((language) => (
           <MenuItem
             key={language}
@@ -52,6 +49,7 @@ export function LanguageSelector({
               setSelectedLang(language);
               handleClose();
             }}
+            selected={language === selectedLang}
           >
             {getLanguageName(language, i18n.language)}
           </MenuItem>
