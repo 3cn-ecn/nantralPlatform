133efa20b081b7e0dcf0c3e7bd35d4e84b71484b
Alexis Delage <alexis.delage@eleves.ec-nantes.fr>
2023-10-17 16:53:22 +0200

    fix: show error when there is errors

--- frontend/src/shared/components/FormErrorAlert/FormErrorAlert.tsx
+++ frontend/src/shared/components/FormErrorAlert/FormErrorAlert.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react';
+
 import { Alert, AlertTitle } from '@mui/material';
 
 import { useTranslation } from '#shared/i18n/useTranslation';
@@ -10,11 +12,19 @@ interface FormErrorAlertProps<T> {
 
 export function FormErrorAlert<T>({ isError, error }: FormErrorAlertProps<T>) {
   const { t } = useTranslation();
+  const alertRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (isError) {
+      // scroll to the alert when it appears
+      alertRef.current?.scrollIntoView();
+    }
+  }, [isError]);
 
   if (!isError) return null;
 
   return (
-    <Alert severity="error" sx={{ mb: 1 }}>
+    <Alert severity="error" sx={{ mb: 1 }} ref={alertRef}>
       <AlertTitle>{t('form.errors.title')}</AlertTitle>
       {!!error?.globalErrors?.length && (
         <ul style={{ margin: 0, paddingInlineStart: 20 }}>
