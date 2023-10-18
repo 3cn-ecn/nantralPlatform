2dde5bc51f8a83ace295a380c49eea5be2042631
Alexis Delage <alexis.delage@eleves.ec-nantes.fr>
2023-10-18 11:08:52 +0200

    fix: remove autofocus

--- frontend/src/pages/Event/EventGrid/EventGridView.page.tsx
+++ frontend/src/pages/Event/EventGrid/EventGridView.page.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 
 import { SearchField } from '#shared/components/FormFields';
 import { Spacer } from '#shared/components/Spacer/Spacer';
-import { useDesktopMode } from '#shared/hooks/useDesktopMode';
 import { useTranslation } from '#shared/i18n/useTranslation';
 
 import { useFilters } from '../hooks/useFilters';
@@ -13,7 +12,6 @@ export default function EventGridViewPage() {
   const { t } = useTranslation();
   const [filters, updateFilters, resetFilters] = useFilters();
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const isDesktopMode = useDesktopMode();
 
   return (
     <>
@@ -23,7 +21,6 @@ export default function EventGridViewPage() {
         size="small"
         placeholder={t('event.search.placeholder')}
         margin="none"
-        autoFocus={isDesktopMode}
       />
       <Spacer vertical={1.5} />
       <FilterBar
