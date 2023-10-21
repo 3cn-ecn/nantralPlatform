6d137b6b1e1cd06a74f8abfac557b47abd80cf2e
rravelli <remi.ravelli@gmail.com>
2023-10-21 20:05:24 +0200

    use react with reminder mails & fix import

--- backend/apps/account/admin.py
+++ backend/apps/account/admin.py
@@ -1,17 +1,13 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.sites.shortcuts import get_current_site
-from django.core import mail
-from django.template.loader import render_to_string
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 from apps.student.admin import StudentInline
+from apps.utils.send_email import send_mass_email
 
-from .models import InvitationLink
-
-User = get_user_model()
+from .models import InvitationLink, User
 
 
 class UppercaseEmailFilter(admin.SimpleListFilter):
@@ -109,30 +105,28 @@ class CustomUserAdmin(UserAdmin):
         "is_superuser",
         "is_active",
         "groups",
+        "invitation",
         UppercaseEmailFilter,
         ECNantesDomainFilter,
         NoPasswordFilter,
     )
 
     @admin.action(description="Send reminder to upgrade account.")
-    def send_reminder(self, request, queryset):
-        connection = mail.get_connection()
+    def send_reminder(self, request, queryset: list[User]):
         current_site = get_current_site(request)
-        mails = []
-        for user in queryset:
-            email_html = render_to_string(
-                "account/mail/reminder_upgrade.html",
-                context={
-                    "tempAccess": user,
+        send_mass_email(
+            template_name="reminder-upgrade",
+            subject="[Nantral Platform] Votre compte expire bientôt !",
+            context_list=[
+                {
+                    "first_name": user.first_name,
                     "deadline": user.invitation.expires_at,
-                    "domain": current_site.domain,
-                },
-            )
-            email = mail.EmailMultiAlternatives(
-                subject="[Nantral Platform] Votre compte expire bientôt !",
-                body=email_html,
-                to=[user.email],
-            )
-            email.attach_alternative(content=email_html, mimetype="text/html")
-            mails.append(email)
-        connection.send_messages(mails)
+                    "change_email_link": f'https://{current_site.domain}{reverse("account:upgrade-permanent")}',
+                }
+                for user in queryset
+                if user.invitation is not None
+            ],
+            recipient_list=[
+                user.email for user in queryset if user.invitation is not None
+            ],
+        )
