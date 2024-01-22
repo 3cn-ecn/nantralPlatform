29911023dbc987186ab345335f8bf7e3c015f63a
Alexis Delage <alexis.delage@eleves.ec-nantes.fr>
2024-01-22 22:21:25 +0100

    chore: clean old code

--- frontend/src/legacy/notification/notificationMenu.tsx
+++
@@ -1,9 +0,0 @@
-import { NotificationMenu } from '#modules/notification/view/NotificationMenu/NotificationMenu';
-
-import { wrapAndRenderLegacyCode } from '../utils/wrapAndRenderLegacyCode';
-
-async function loadNotificationMenu() {
-  wrapAndRenderLegacyCode(<NotificationMenu />, 'notificationPanel');
-}
-
-export default loadNotificationMenu;
