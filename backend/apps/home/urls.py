435c1cdda9d532205f30b785e58b619d8a989af1
Zakaria EL KHAYDER <zelkhayder@gmail.com>
2024-02-05 23:20:35 +0100

    Added base Feedback adn Suggestion pages

--- backend/apps/home/urls.py
+++ backend/apps/home/urls.py
@@ -6,7 +6,7 @@ app_name = "home"
 
 urlpatterns = [
     # pages
-    path("suggestions/", views.SuggestionView.as_view(), name="suggestions"),
+    # path("suggestions/", views.SuggestionView.as_view(), name="suggestions"),
     # shortcuts
     path("me/", views.current_user_page_view, name="me"),
     path("my_coloc/", views.current_user_roommates_view, name="my_coloc"),
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -19,6 +19,8 @@ const HomePage = lazy(() => import('#pages/Home/Home.page'));
 const LegalNoticePage = lazy(() => import('#pages/LegalNotice/Legal.page'));
 const NotFoundPage = lazy(() => import('#pages/NotFound/NotFound.page'));
 const Signature = lazy(() => import('#pages/Signature/Signature.page'));
+const FeedbackPage = lazy(() => import('#pages/Feedback/Feedback.page'));
+const SuggestionPage = lazy(() => import('#pages/Feedback/Suggestion.page'));
 
 // create a fake t function so that vscode can show translations in editor
 const t = (key: string) => key;
@@ -69,6 +71,21 @@ export const router = createBrowserRouter([
         path: '/legal-notice',
         element: <LegalNoticePage />,
         handle: { crumb: t('breadcrumbs.legal.index') },
+      },
+      {
+        path: '/feedback',
+        handle: { crumb: t('breadcrumbs.feedback.index') },
+        children: [
+          {
+            index: true,
+            element: <FeedbackPage />,
+          },
+          {
+            path: 'suggestion',
+            element: <SuggestionPage />,
+            handle: { crumb: t('breadcrumbs.feedback.suggestion') },
+          },
+        ],
       },
       {
         path: '*',
