3586e052dd87acf3f0ee62132b2764ed94038906
rravelli <remi.ravelli@gmail.com>
2024-02-10 15:19:30 +0100

    chore: remove unused views and tests

--- backend/apps/account/urls.py
+++ backend/apps/account/urls.py
@@ -1,42 +1,17 @@
 from django.urls import path
 
 from .views import (
-    AuthView,
     ConfirmEmail,
     ConfirmUser,
-    ForgottenPassView,
-    LogoutView,
     PasswordResetConfirmCustomView,
     PermanentAccountUpgradeView,
-    RegistrationChoice,
-    RegistrationView,
-    TemporaryRegistrationChoice,
-    TemporaryRegistrationView,
     redirect_to_student,
 )
 
 app_name = "account"
 
 urlpatterns = [
-    path("login/", AuthView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
     path("login/confirm_email/", ConfirmEmail.as_view(), name="confirm-email"),
-    path(
-        "registration/choice/",
-        RegistrationChoice.as_view(),
-        name="registration-choice",
-    ),
-    path(
-        "registration/choice/<uuid:invite_id>/",
-        TemporaryRegistrationChoice.as_view(),
-        name="temp-registration-choice",
-    ),
-    path("registration/", RegistrationView.as_view(), name="registration"),
-    path(
-        "registration/<uuid:invite_id>/",
-        TemporaryRegistrationView.as_view(),
-        name="temporary-registration",
-    ),
     path(
         "activate/<slug:uidb64>/<slug:token>/",
         ConfirmUser.as_view(),
@@ -47,7 +22,6 @@ urlpatterns = [
         PermanentAccountUpgradeView.as_view(),
         name="upgrade-permanent",
     ),
-    path("forgotten/", ForgottenPassView.as_view(), name="forgotten_pass"),
     path(
         "reset_pass/<slug:token>/",
         PasswordResetConfirmCustomView.as_view(),
--- backend/apps/account/views.py
+++ backend/apps/account/views.py
@@ -1,15 +1,13 @@
-import uuid
-from typing import Any, Dict
-from urllib.parse import urlparse
+from typing import Any
 
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login
 from django.contrib.auth.forms import SetPasswordForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.forms.forms import BaseForm
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
 from django.utils.encoding import force_str
 from django.utils.http import urlsafe_base64_decode
 from django.utils.translation import gettext as _
@@ -19,21 +17,14 @@ from django.views.generic import FormView, TemplateView
 
 from django_rest_passwordreset.views import (
     ResetPasswordConfirmViewSet,
-    ResetPasswordRequestTokenViewSet,
     ResetPasswordValidateTokenViewSet,
 )
 from rest_framework.exceptions import ValidationError
 
 from apps.student.models import Student
 
-from .forms import (
-    ForgottenPassForm,
-    LoginForm,
-    SignUpForm,
-    TemporaryRequestSignUpForm,
-    UpgradePermanentAccountForm,
-)
-from .models import InvitationLink, User
+from .forms import SignUpForm, UpgradePermanentAccountForm
+from .models import User
 from .tokens import account_activation_token
 from .utils import send_email_confirmation, user_creation
 
@@ -45,56 +36,6 @@ class RegistrationView(FormView):
     def form_valid(self, form):
         user_creation(form, self.request)
         return redirect("home:home")
-
-
-class TemporaryRegistrationView(FormView):
-    form_class = TemporaryRequestSignUpForm
-    template_name = "account/temporary_registration.html"
-
-    def get(self, request, invite_id: uuid.UUID, *args, **kwargs):
-        self.invitation = InvitationLink.objects.filter(id=invite_id).first()
-        # Do not allow to use this view if invitation is expired
-        if self.invitation is None or not self.invitation.is_valid():
-            messages.error(
-                request, "Invitation invalide : le lien d'invitation a expiré."
-            )
-            return redirect("account:registration-choice")
-
-        return super().get(request, *args, **kwargs)
-
-    def post(
-        self,
-        request: HttpRequest,
-        invite_id: uuid.UUID,
-        *args: str,
-        **kwargs: Any,
-    ) -> HttpResponse:
-        self.invitation = InvitationLink.objects.filter(id=invite_id).first()
-        if self.invitation is None or not self.invitation.is_valid():
-            messages.error(
-                request, "Invitation invalide : le lien d'invitation a expiré."
-            )
-            return redirect("account:registration-choice")
-
-        return super().post(request, *args, **kwargs)
-
-    def get_initial(self) -> Dict[str, Any]:
-        initial = super().get_initial()
-        initial["invite_id"] = self.kwargs["invite_id"]
-        return initial
-
-    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-
-        context["invitation"] = self.invitation
-        context["DEADLINE_TEMPORARY_REGISTRATION"] = self.invitation.expires_at
-        return context
-
-    def form_valid(self, form) -> HttpResponse:
-        invite_id = self.request.path.split("/")[-2]
-        invitation = InvitationLink.objects.get(id=invite_id)
-        user_creation(form, self.request, invitation=invitation)
-        return redirect("account:login")
 
 
 class ConfirmUser(View):
@@ -127,105 +68,6 @@ class ConfirmUser(View):
             return redirect("home:home")
         else:
             return render(self.request, "account/activation_invalid.html")
-
-
-class AuthView(FormView):
-    template_name = "account/login.html"
-    form_class = LoginForm
-
-    def get(self, request):
-        if request.user.is_authenticated:
-            # we send back the user to where he wanted to go or to home page
-            url = self.request.GET.get("next", "/")
-            parsed_uri = urlparse(url)
-            if parsed_uri.scheme != "" or parsed_uri.netloc != "":
-                url = "/"
-            user = request.user
-            message = (
-                "Vous êtes déjà connecté en tant que "
-                f"{user.first_name.title()}."
-            )
-            messages.warning(request, message)
-            return redirect(url)
-
-        return super().get(self, request)
-
-    def form_invalid(self, form):
-        message = "Veuillez vous connecter avec votre adresse mail ECN."
-        messages.warning(self.request, message)
-        return redirect("account:login")
-
-    def form_valid(self, form):
-        email = form.cleaned_data["email"]
-        password = form.cleaned_data["password"]
-        user: User = authenticate(self.request, email=email, password=password)
-
-        url = self.request.GET.get("next", "/")
-        parsed_uri = urlparse(url)
-        if parsed_uri.scheme != "" or parsed_uri.netloc != "":
-            url = "/"
-
-        # Wrong credentials or expired invitation
-        if user is None or (
-            user.invitation is not None and not user.invitation.is_valid()
-        ):
-            messages.error(
-                self.request, "Identifiant inconnu ou mot de passe invalide."
-            )
-            return redirect("account:login")
-
-        # Not verified email
-        if not user.is_email_valid:
-            self.request.session["email"] = email
-            return redirect("account:confirm-email")
-
-        # Temporary account
-        if user.invitation is not None and user.invitation.is_valid():
-            message = (
-                "Votre compte n'est pas encore définitif. "
-                'Veuillez le valider <a href="'
-                f'{reverse("account:upgrade-permanent")}">ici</a>. '
-                "Attention après le "
-                f"{user.invitation.expires_at.strftime('%a')} vous ne "
-                "pourrez plus vous connecter si vous n'avez pas "
-                "renseigné votre adresse Centrale."
-            )
-            messages.warning(self.request, message)
-            login(self.request, user)
-            # We send back the user to where he wanted to go or to home page
-            return redirect(url)
-
-        # Normal case
-        login(self.request, user)
-        # We send back the user to where he wanted to go or to home page
-        return redirect(url)
-
-
-class LogoutView(View):
-    def get(self, request):
-        logout(request)
-        messages.success(request, "Vous avez été déconnecté.")
-        return redirect("account:login")
-
-
-class ForgottenPassView(FormView):
-    form_class = ForgottenPassForm
-    template_name = "account/forgotten_pass.html"
-
-    def form_valid(self, form):
-        user = User.objects.filter(email=form.cleaned_data["email"]).first()
-        if user is not None:
-            self.request.data = {"email": user.email}
-            ResetPasswordRequestTokenViewSet(request=self.request).post(
-                request=self.request
-            )
-        messages.success(
-            self.request,
-            "Un email vous a été envoyé. Si vous ne recevez rien "
-            "dans les 5 prochaines minutes, cela signifie qu'aucun compte "
-            "n'est enregistré avec cette adresse email.",
-        )
-        return redirect("account:login")
 
 
 class PasswordResetConfirmCustomView(FormView):
@@ -305,19 +147,6 @@ class PermanentAccountUpgradeView(LoginRequiredMixin, FormView):
             self.request.user, self.request, send_to=new_email
         )
         return super().form_valid(form)
-
-
-class RegistrationChoice(TemplateView):
-    template_name = "account/registration-choice.html"
-
-
-class TemporaryRegistrationChoice(TemplateView):
-    template_name = "account/temp-registration-choice.html"
-
-    def get_context_data(self, invite_id, **kwargs: Any) -> Dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        context["invite_id"] = invite_id
-        return context
 
 
 class ConfirmEmail(TemplateView):
