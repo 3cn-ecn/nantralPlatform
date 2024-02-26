6b056a967a59675562398653222d3b02bbb6bd24
rravelli <remi.ravelli@gmail.com>
2024-02-26 13:27:19 +0100

    feat: (backend) social link api

--- backend/apps/sociallink/serializers.py
+++ backend/apps/sociallink/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from apps.group.models import Group
 
@@ -12,24 +13,36 @@ class NetworkSerializer(serializers.ModelSerializer):
 
 
 class SocialLinkSerializer(serializers.ModelSerializer):
-    network = NetworkSerializer()
+    class Meta:
+        model = SocialLink
+        fields = ["id", "uri", "network", "label"]
+        depth = 1
 
+
+class SocialLinkUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = SocialLink
         fields = ["id", "uri", "network", "label"]
 
 
-class SocialLinWriteSerializer(serializers.ModelSerializer):
+class SocialLinkCreateSerializer(serializers.ModelSerializer):
+    group = serializers.SlugField(write_only=True)
+
     class Meta:
         model = SocialLink
-        fields = ["uri", "network", "label", "network"]
+        fields = ["uri", "network", "label", "network", "group"]
 
-    def create(self, validated_data):
-        social_link = super().create(validated_data)
-        view = self.context.get("view")
-        group_slug = view and view.kwargs.get("group_slug")
-        if group_slug:
-            group = Group.objects.get(slug=group_slug)
+    def validate_group(self, val: str):
+        if not Group.objects.filter(slug=val).exists():
+            raise ValidationError("Group slug does not exist")
+        return val
+
+    def create(self, validated_data: dict):
+        group = validated_data.pop("group", None)
+        social_link = SocialLink.objects.create(**validated_data)
+        if group:
+            group = Group.objects.get(slug=group)
             group.social_links.add(social_link)
             group.save()
+
         return social_link
