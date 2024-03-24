6cc4022c60366e5c49190019cdab04b36fee6b6f
Alexis Delage <alexis.delage@eleves.ec-nantes.fr>
2024-03-24 14:12:00 +0100

    feat: create factories

---
+++ backend/apps/account/factories.py
@@ -0,0 +1,28 @@
+from django.db.models.signals import post_save
+
+import factory
+from factory.django import DjangoModelFactory
+
+from .models import User
+
+
+@factory.django.mute_signals(post_save)  # Use this because OneToOneField
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = User
+        skip_postgeneration_save = True
+
+    email = factory.Faker("email", domain="fake.ec-nantes.fr")
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    is_email_valid = True
+    password = factory.django.Password("pass")
+    username = factory.LazyAttribute(
+        lambda obj: f"{obj.first_name.lower()}.{obj.last_name.lower()}"
+    )
+
+    # Declare the related student factory because of the OneToOneField
+    # see: https://factoryboy.readthedocs.io/en/stable/recipes.html#example-django-s-profile  # noqa: E501
+    student = factory.RelatedFactory(
+        "apps.student.factories.StudentFactory", factory_related_name="user"
+    )
