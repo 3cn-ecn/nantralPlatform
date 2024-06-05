07c28e4890cd0c628119faf801e0922d282c65e4
Alexis Delage <alexis.delage@eleves.ec-nantes.fr>
2024-06-05 21:30:57 +0200

    chore: remove old view

--- backend/apps/home/api_urls.py
+++ backend/apps/home/api_urls.py
@@ -4,8 +4,6 @@ from .api_views import CreateFeedbackView
 
 app_name = "api_home"
 
-# rooter for API: it creates all urls for a viewSet at once
-# see https://www.django-rest-framework.org/api-guide/routers/#simplerouter
 
 urlpatterns = [
     path("feedback/", CreateFeedbackView.as_view(), name="feedback"),
