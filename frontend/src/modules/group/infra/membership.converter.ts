11015b2bbe8183adfb867d4b55f54626b5fe1bcf
rravelli <112650656+rravelli@users.noreply.github.com>
2024-07-23 14:04:21 +0200

    Update frontend/src/modules/group/infra/membership.converter.ts
    Co-authored-by: Alexis Delage <alexidelage+github@gmail.com>

--- frontend/src/modules/group/infra/membership.converter.ts
+++ frontend/src/modules/group/infra/membership.converter.ts
@@ -3,8 +3,8 @@ import { MembershipFormDTO } from './membership.dto';
 
 export function convertMembershipForm(form: MembershipForm): MembershipFormDTO {
   return {
-    begin_date: form.beginDate.toISOString().slice(0, 10) || '',
-    end_date: form.endDate.toISOString().slice(0, 10) || '',
+    begin_date: form.beginDate.toISOString().split('T')[0] || '',
+    end_date: form.endDate.toISOString().split('T')[0] || '',
     description: form.description,
     group: form.group,
     student: form.student,
