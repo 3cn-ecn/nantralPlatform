033c7f5aee3c9f093c720db46ba1af8c12b01fd0
rravelli <remi.ravelli@gmail.com>
2024-08-13 15:21:34 +0200

    fix: add extra crumbs

--- frontend/src/shared/routes/publicRoutes.tsx
+++ frontend/src/shared/routes/publicRoutes.tsx
@@ -1,6 +1,8 @@
 import { lazy } from 'react';
-import { RouteObject } from 'react-router-dom';
+import { LoaderFunctionArgs, RouteObject } from 'react-router-dom';
 
+import { getGroupDetailsApi } from '#modules/group/api/getGroupDetails.api';
+import { getGroupTypeDetailsApi } from '#modules/group/api/getGroupTypeDetails.api';
 import { PageTemplate } from '#shared/components/PageTemplate/PageTemplate';
 
 const LegalNoticePage = lazy(() => import('#pages/LegalNotice/Legal.page'));
@@ -9,6 +11,53 @@ const GroupDetailsPage = lazy(
   () => import('#pages/GroupDetails/GroupDetails.page'),
 );
 const t = (key: string) => key;
+
+async function groupTypeLoader(obj: LoaderFunctionArgs<unknown>) {
+  const type = new URL(obj.request.url).searchParams.get('type');
+  if (!type) {
+    return {};
+  }
+  try {
+    const groupType = await getGroupTypeDetailsApi(type);
+    return {
+      extraCrumb: [
+        {
+          id: groupType.slug,
+          label: groupType.name,
+          path: obj.request.url,
+        },
+      ],
+    };
+  } catch {
+    return {};
+  }
+}
+
+async function groupDetailsLoader(obj: LoaderFunctionArgs<unknown>) {
+  const type = obj.params.type;
+  if (!type) {
+    return {};
+  }
+  try {
+    const group = await getGroupDetailsApi(type.slice(1));
+    return {
+      extraCrumb: [
+        {
+          id: group.groupType.slug,
+          label: group.groupType.name,
+          path: `/group/?type=${group.groupType.slug}`,
+        },
+        {
+          id: group.id,
+          label: group.shortName,
+          path: group.url,
+        },
+      ],
+    };
+  } catch {
+    return {};
+  }
+}
 
 export const publicRoutes: RouteObject = {
   element: <PageTemplate />,
@@ -23,13 +72,14 @@ export const publicRoutes: RouteObject = {
       handle: { crumb: t('breadcrumbs.group.index') },
       children: [
         {
+          loader: groupTypeLoader,
           path: '',
           element: <GroupPage />,
         },
         {
+          loader: groupDetailsLoader,
           path: ':type',
           element: <GroupDetailsPage />,
-          handle: { crumb: t('breadcrumbs.group.details') },
         },
       ],
     },
