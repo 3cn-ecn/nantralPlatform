fde94f65faa5a2550ff2f04562f00e2548270c45
rravelli <remi.ravelli@gmail.com>
2024-09-12 15:17:25 +0200

    refactor: social link group/student api

--- backend/apps/sociallink/api_views.py
+++ backend/apps/sociallink/api_views.py
@@ -3,14 +3,23 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from .models import SocialLink
-from .permissions import SocialLinkPermission
-from .serializers import GroupSocialLinkSerializer
+from .permissions import GroupSocialLinkPermission, UserSocialLinkPermission
+from .serializers import GroupSocialLinkSerializer, UserSocialLinkSerializer
 
 
 class GroupSocialLinkViewSet(ModelViewSet):
     queryset = SocialLink.objects.all()
-    permission_classes = [SocialLinkPermission, IsAuthenticated]
+    permission_classes = [GroupSocialLinkPermission, IsAuthenticated]
     serializer_class = GroupSocialLinkSerializer
 
     def list(self, request):
-        raise MethodNotAllowed("GET")
+        raise MethodNotAllowed("GET", detail='Method "GET" not allowed')
+
+
+class UserSocialLinkViewSet(ModelViewSet):
+    queryset = SocialLink.objects.all()
+    permission_classes = [UserSocialLinkPermission, IsAuthenticated]
+    serializer_class = [UserSocialLinkSerializer]
+
+    def list(self, request):
+        raise MethodNotAllowed("GET", detail='Method "GET" not allowed')
--- backend/apps/sociallink/permissions.py
+++ backend/apps/sociallink/permissions.py
@@ -1,13 +1,18 @@
+from typing import TYPE_CHECKING
+
 from rest_framework import permissions
-from rest_framework.request import HttpRequest
+from rest_framework.request import Request
 
 from apps.group.models import Group
 
 from .models import SocialLink
 
+if TYPE_CHECKING:
+    from apps.student.models import Student
 
-class SocialLinkPermission(permissions.BasePermission):
-    def has_permission(self, request: HttpRequest, view):
+
+class GroupSocialLinkPermission(permissions.BasePermission):
+    def has_permission(self, request: Request, view):
         user = request.user
 
         if view.action == "create":
@@ -18,7 +23,7 @@ class SocialLinkPermission(permissions.BasePermission):
         return True
 
     def has_object_permission(
-        self, request: HttpRequest, view, obj: SocialLink
+        self, request: Request, view, obj: SocialLink
     ):
         if request.method in permissions.SAFE_METHODS:
             return True
@@ -27,3 +32,9 @@ class SocialLinkPermission(permissions.BasePermission):
         group_set = obj.group_set.all()  # A social link could be associated with multiple groups in theory (but not in practice)
 
         return any(group.is_admin(user) for group in group_set)
+
+
+class UserSocialLinkPermission(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj: SocialLink):
+        student: Student = request.user.student
+        return student.social_links.contains(obj)
