a9fef1522616d2d4dcfe3c90e36157656b22b864
Charles Zablit <zablitcharles@gmail.com>
2025-03-29 17:26:27 +0100

    fix email connection

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -19,8 +19,8 @@ else:
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_PORT = env("EMAIL_PORT")
-EMAIL_USE_SSL=False
-EMAIL_USE_TLS=True
+EMAIL_USE_SSL=True
+EMAIL_USE_TLS=False
 # No need to authenticate on localhost
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
