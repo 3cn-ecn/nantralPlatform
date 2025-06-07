58583262661a728ee4480ec6833dec4046a79340
Jean-Benoît Grimaldi <jbenoit.grimaldi+github@gmail.com>
2025-06-07 22:53:38 +0200

    feat: add an authentication backend for matrix
     * add an API view to authenticate users
     * add .well-known for matrix server (TODO: put the actual server address)
     * update username field in User model to match matrix requirements
     * update username generation to match matrix requirements

---
+++ backend/apps/account/matrix_views.py
@@ -0,0 +1,55 @@
+from django.conf import settings
+from django.contrib.auth import authenticate
+
+from rest_framework import (
+    authentication,
+    exceptions,
+    permissions,
+    response,
+    views,
+)
+
+from apps.account.models import User
+
+
+class CheckCredentials(views.APIView):
+    """
+    A class to authenticate users for matrix servers using a custom auth provider
+    """
+
+    # Basic authentication to avoid CSRF
+    # This is not a problem because we do not log the user in
+    authentication_classes = [authentication.BasicAuthentication]
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get("user").get("id")
+        password = request.data.get("user").get("password")
+        email = request.data.get("user").get("email")
+
+        # Check authentication method (email, matrix id or username)
+        if username:
+            if username[0] == "@":  # Check for matrix id and convert to username
+                username = username.split(":")[0][1:]
+            email = User.objects.get(username=username).email
+
+        user = authenticate(username=email, password=password)
+
+        # Check if the user account exists and is valid
+        if (user is None) or (user.invitation is not None and not user.invitation.is_valid()) or (not user.is_email_valid):
+            raise exceptions.AuthenticationFailed({"auth": {"success": False}})
+
+        # Return user data
+        return response.Response({ "auth": {
+            "success": True,
+            "mxid": f"@{user.username}:{settings.MATRIX_SERVER_NAME}",
+            "profile": {
+                "display_name": user.student.name,
+                "three_pids": [
+                    {
+                        "medium": "email",
+                        "address": user.email
+                    }
+                ]
+            }
+        }})
