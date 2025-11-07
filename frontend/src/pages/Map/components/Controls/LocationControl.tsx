d1c017124c388444d759e14a528e1580812933e8
Jean-Benoît Grimaldi <jbenoit.grimaldi+github@gmail.com>
2025-11-07 11:22:24 +0100

    Improve geolocate

--- frontend/src/pages/Map/components/Controls/LocationControl.tsx
+++ frontend/src/pages/Map/components/Controls/LocationControl.tsx
@@ -8,6 +8,7 @@ import {
 import { CircularProgress, IconButton, Tooltip } from '@mui/material';
 import type { GeolocateControl as GeolocateControlType } from 'mapbox-gl';
 
+import { useToast } from '#shared/context/Toast.context';
 import { useTranslation } from '#shared/i18n/useTranslation';
 
 export function LocationControl({
@@ -16,6 +17,7 @@ export function LocationControl({
   portalContainer?: HTMLElement | null;
 }) {
   const { t } = useTranslation();
+  const showToast = useToast();
   const geoControlRef = useRef<GeolocateControlType>(null);
 
   const [isLocating, setIsLocating] = useState(false);
@@ -32,23 +34,27 @@ export function LocationControl({
         placement={'left'}
         PopperProps={{ container: portalContainer }}
       >
-        <IconButton
-          size="small"
-          color="primary"
-          onClick={() => {
-            setIsLocating(true);
-            geoControlRef.current?.trigger();
-          }}
-          disabled={isLocating}
-        >
-          {isLocating ? (
-            <CircularProgress size={20} color="inherit" />
-          ) : followPosition ? (
-            <GpsFixedIcon />
-          ) : (
-            <GpsNotFixedIcon />
-          )}
-        </IconButton>
+        {/* use `span` because of disabled button and tooltip */}
+        <span>
+          <IconButton
+            size="small"
+            color="primary"
+            onClick={() => {
+              if (geoControlRef.current?._lastKnownPosition === undefined)
+                setIsLocating(true);
+              geoControlRef.current?.trigger();
+            }}
+            disabled={isLocating}
+          >
+            {isLocating ? (
+              <CircularProgress size={20} color="inherit" />
+            ) : followPosition ? (
+              <GpsFixedIcon />
+            ) : (
+              <GpsNotFixedIcon />
+            )}
+          </IconButton>
+        </span>
       </Tooltip>
       <GeolocateControl
         ref={geoControlRef}
@@ -59,6 +65,19 @@ export function LocationControl({
         onTrackUserLocationEnd={() => {
           setFollowPosition(false);
           setIsLocating(false);
+        }}
+        onError={(err) => {
+          setIsLocating(false);
+          setFollowPosition(false);
+          showToast({
+            variant: 'error',
+            message:
+              err.code === err.PERMISSION_DENIED
+                ? t('map.controls.permissionDenied')
+                : err.code === err.POSITION_UNAVAILABLE
+                  ? t('map.controls.positionUnavailable')
+                  : t('map.controls.timeout'),
+          });
         }}
         style={{ display: 'none' }} // hide the control to use our custom one
       />
