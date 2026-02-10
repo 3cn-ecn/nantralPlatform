83301bfa7d305c3df828f229d108cf78d98e2a45
Jean-Benoît Grimaldi <jbenoit.grimaldi+github@gmail.com>
2026-02-10 18:30:21 +0100

    chores: remove unused StudentPermission

--- backend/apps/account/permissions.py
+++ backend/apps/account/permissions.py
@@ -3,13 +3,6 @@ from rest_framework import permissions
 from apps.account.models import Email, User
 
 
-class StudentPermission(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj: User) -> bool:
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return request.user == obj
-
-
 class CanEditProfileOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
