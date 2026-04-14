7598349422c1e5f3ac9e1bef8f23a5377227bc4c
Jean-Benoît Grimaldi <jbenoit.grimaldi+github@gmail.com>
2026-04-14 13:39:46 +0200

    fix: allow deleting latest history entry

--- backend/apps/group/api_views.py
+++ backend/apps/group/api_views.py
@@ -1,3 +1,4 @@
+import requests
 from django.conf import settings
 from django.db.models import Count, F, Q, QuerySet
 from django.http.request import QueryDict
@@ -6,8 +7,6 @@ from django.template.loader import render_to_string
 from django.utils import timezone
 from django.utils.dateparse import parse_datetime
 from django.utils.translation import gettext as _
-
-import requests
 from requests import Request
 from rest_framework import (
     decorators,
@@ -25,7 +24,6 @@ from rest_framework.settings import api_settings
 
 from apps.utils.discord import respond_admin_request, send_admin_request
 from apps.utils.parse import parse_bool
-
 from .models import Group, GroupType, Label, Membership
 from .permissions import (
     AdminRequestListPermission,
@@ -326,15 +324,6 @@ class HistoryGroupViewSet(
                 code="invalid",
             )
 
-        if (
-            group
-            and group.history.most_recent().history_id == history.history_id
-        ):
-            raise BadRequestException(
-                _("You cannot delete the most recent version of a group."),
-                code="invalid",
-            )
-
         history.delete()
         return response.Response(status=status.HTTP_204_NO_CONTENT)
 
@@ -490,7 +479,7 @@ class MembershipViewSet(viewsets.ModelViewSet):
         )
         # check that memberships are from same group
         if (lower and lower.group != member.group) or self.query_params.get(
-            "group"
+                "group"
         ) != member.group.slug:
             raise exceptions.ValidationError(
                 _("All memberships objects must be from the same group."),
@@ -615,8 +604,8 @@ class MembershipViewSet(viewsets.ModelViewSet):
             )
             membership.user.email_user(
                 subject=(
-                    _("Your admin request for %(group)s has been accepted.")
-                    % {"group": membership.group.name}
+                        _("Your admin request for %(group)s has been accepted.")
+                        % {"group": membership.group.name}
                 ),
                 message=mail,
                 html_message=mail,
@@ -636,7 +625,7 @@ class MembershipViewSet(viewsets.ModelViewSet):
         return response.Response(
             {
                 "message": _("The user %(user)s is now admin!")
-                % {"user": membership.user}
+                           % {"user": membership.user}
             },
             status=status.HTTP_202_ACCEPTED,
         )
@@ -666,8 +655,8 @@ class MembershipViewSet(viewsets.ModelViewSet):
             )
             membership.user.email_user(
                 subject=(
-                    _("Your admin request for %(group)s has been denied.")
-                    % {"group": membership.group.name}
+                        _("Your admin request for %(group)s has been denied.")
+                        % {"group": membership.group.name}
                 ),
                 message=mail,
                 html_message=mail,
@@ -687,7 +676,7 @@ class MembershipViewSet(viewsets.ModelViewSet):
         return response.Response(
             {
                 "message": _("The admin request from %(user)s has been denied.")
-                % {"user": membership.user}
+                           % {"user": membership.user}
             },
             status=status.HTTP_202_ACCEPTED,
         )
